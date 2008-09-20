ef0cbc5f3beb576f1418f44168c69fda87c31c61
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-20 20:15:16 +0000

    Added P2pChat, a port of Microsoft's WCF Chat on MSDN (http://msdn.microsoft.com/en-us/library/bb690929.aspx) written in VB.

---
+++ trunk/P2pChat/WcfChatClient/ChatContract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.PeerResolvers;
+
+namespace Microsoft.ServiceModel.Samples
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples", CallbackContract = typeof(IChat))]
+    public interface IChat
+    {
+        [OperationContract(IsOneWay = true)]
+        void Join(string member);
+        [OperationContract(IsOneWay = true)]
+        void Chat(string member, string message);
+        [OperationContract(IsOneWay = true)]
+        void Leave(string member);
+    }
+
+    public interface IChatChannel : IChat, IClientChannel
+    {}
+}
