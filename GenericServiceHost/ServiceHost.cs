19506fe59f26a9ac0bf4b6dcbcafd7ffaa14770e
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-22 15:19:51 +0000

    Added GenericServiceHost

---
+++ GenericServiceHost/ServiceHost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace System.ServiceModel
+{
+    public class ServiceHost<T> : ServiceHost
+    {
+        public ServiceHost() : base(typeof(T)) 
+        { }
+        public ServiceHost(params string[] baseAddresses)
+            : base(typeof(T), Convert(baseAddresses))
+        { }
+        public ServiceHost(params Uri[] baseAddresses)
+            : base(typeof(T), baseAddresses)
+        { }
+        static Uri[] Convert(string[] baseAddresses)
+        {
+            Converter<string, Uri> convert = delegate(string address)
+            {
+                return new Uri(address);
+            };
+            return Array.ConvertAll(baseAddresses, convert);
+        }
+    }
+}
