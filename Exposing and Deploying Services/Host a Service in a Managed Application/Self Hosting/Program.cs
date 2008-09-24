a7b207ac7b4fe7e8f16db247811559df4d7e2661
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-24 11:57:39 +0000

    Add Self Hosting example.

---
+++ Exposing and Deploying Services/Host a Service in a Managed Application/Self Hosting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SelfHosting {
+
+    class Program {
+
+        static void Main(string[] args) {
+            ServiceHost host = new ServiceHost(typeof(MyService));
+            host.Open();
+
+            Console.ReadKey(true);
+
+            host.Close();
+        }
+    }
+
+    [ServiceContract]
+    interface IMyContract {
+
+        [OperationContract]
+        string MyOperation();
+    }
+
+    class MyService : IMyContract {
+
+        public string MyOperation() {
+            return "MyResult";
+        }
+    }
+}
