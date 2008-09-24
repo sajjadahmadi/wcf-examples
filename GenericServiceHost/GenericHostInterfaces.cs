0e7b84038e4fb65f214af578aec372106dce46a0
mark.alan.good <mark.alan.good@gmail.com>
2008-09-24 01:37:32 +0000

    Added IEnableMetaDataExchange and implemented it on ServiceHost<T>

---
+++ GenericServiceHost/GenericHostInterfaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ServiceModel
+{
+   interface IEnableMetaDataExchange
+   {
+      bool EnableMetaDataExchange { get; set; }
+      bool HasMexEndpoint { get; }
+      void AddAllMexEndPoints();
+   }
+}
