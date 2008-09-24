009af2414870b127ceda33cc41f2353c6e99db87
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-24 11:37:28 +0000

    Add simple IIS Hosting example.

---
+++ trunk/ExposingAndDeployingServices/HostAServiceOnAWebServer/IISHosting/Example.svc.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace IISHosting {
+
+    [ServiceContract]
+    public interface IMyContract {
+
+        [OperationContract]
+        string MyOperation();
+    }
+
+    public class MyService : IMyContract {
+
+        public string MyOperation() {
+            return "MyResult";
+        }
+    }
+}
