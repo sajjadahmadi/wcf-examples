b4815479ba708799b8e10f2771ae1bc2edb480ad
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-25 02:41:08 +0000

    Add Basic Mex examples.

---
+++ trunk/Exposing and Deploying Services/Create and Configure Service Endpoints/Basic Mex/Administrative Configuration/Contracts.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+namespace AdministrativeConfiguration {
+
+    [ServiceContract]
+    interface IMyContract {
+
+        [OperationContract]
+        string MyOperation();
+    }
+
+    class MyService : IMyContract {
+
+        public string MyOperation() {
+            return "MyResult";
+        }
+    }
+}
