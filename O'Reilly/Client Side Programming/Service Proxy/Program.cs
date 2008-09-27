a653e168c1f34bea42b1557745a2789f259461ab
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-27 21:41:45 +0000

    Added the book examples for Self Hosting and Service Proxy

---
+++ O'Reilly/Client Side Programming/Service Proxy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceProxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyServiceReference.MyContractClient proxy = new MyServiceReference.MyContractClient();
+            Console.WriteLine( proxy.MyOperation());
+            Console.ReadKey(true);
+            proxy.Close();
+        }
+    }
+}
