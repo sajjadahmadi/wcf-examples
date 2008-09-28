755b7dcf9318c1cd9ebfd743267b147ba8b76133
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-28 11:47:26 +0000

    Moved Client Side Programming and Hosting to 01-Essentials

---
+++ trunk/O'Reilly/01-Essentials/Hosting/Self Hosting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ServiceModel;
+using System.Diagnostics;
+
+namespace SelfHosting {
+
+    class Program {
+
+        static void Main(string[] args) {
+            ServiceHost host = new ServiceHost(typeof(MyService));
+            host.Open();
+            Console.WriteLine("Service Host: {0}", host.State);
+
+            var iePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Internet Explorer\\IEXPLORE.EXE");
+            Process.Start(iePath, "http://localhost:8000");
+            Console.ReadKey(true);
+
+            host.Close();
+        }
+    }
+
+    [ServiceContract]
+    interface IMyContract {
+
+        [OperationContract]
+        string MyOperation();
+    }
+
+    class MyService : IMyContract {
+
+        public string MyOperation() {
+            return "MyResult";
+        }
+    }
+}
