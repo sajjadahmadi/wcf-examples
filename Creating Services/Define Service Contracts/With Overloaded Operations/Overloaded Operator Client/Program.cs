9eb101f3b44c78a9ae5ed65a9d7b0fa4431cb9f4
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-09-29 13:33:57 +0000

    Add example of overloading service operations.

---
+++ Creating Services/Define Service Contracts/With Overloaded Operations/Overloaded Operator Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Overloaded_Operator_Client {
+
+    class Program {
+
+        static void Main(string[] args) {
+            var client = new CalculatorService.CalculatorClient();
+            
+            // Unmodified client code
+            //var n1 = client.AddInt(1, 2);
+            //var n2 = client.AddDouble(1.0, 2.0);
+
+            // Modified client code
+            var n1 = client.Add(1, 2);
+            var n2 = client.Add(1.0, 2.0);
+            Console.WriteLine("n1: {0}", n1);
+            Console.WriteLine("n2: {0}", n2);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Creating Services/Define Service Contracts/With Overloaded Operations/With Overloaded Operations/Contracts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WithOverloadedOperations {
+
+    [ServiceContract]
+    interface ICalculator {
+        
+        [OperationContract(Name = "AddInt")]
+        Int32 Add(Int32 arg1, Int32 arg2);
+
+        [OperationContract(Name = "AddDouble")]
+        double Add(double arg1, double arg2);
+    }
+
+    class Calculator : ICalculator {
+
+        public int Add(int arg1, int arg2) {
+            return arg1 + arg2;
+        }
+
+        public double Add(double arg1, double arg2) {
+            return arg1 + arg2;
+        }
+    }
+}
