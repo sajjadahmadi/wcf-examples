fb0aa2fb175b1a7bebd92161b4f2099842ca0f74
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-04 02:05:45 +0000

    Added MetadataHelper

---
+++ trunk/System.ServiceModel.Extensions/System.ServiceModel.Extensions/MetadataHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+
+namespace System.ServiceModel.Extensions
+{
+    class MetadataHelper
+    {
+        const int MessageSizeMultiplier = 5; 
+
+        static ServiceEndpointCollection GetEndpoints(
+            string mexAddress,
+            BindingElement bindingElement)
+        {
+            CustomBinding binding = new CustomBinding(bindingElement);
+
+            MetadataExchangeClient MEXClient = new MetadataExchangeClient(binding);
+            MetadataSet metadata = MEXClient.GetMetadata(new EndpointAddress(mexAddress));
+
+            MetadataImporter importer = new WsdlImporter(metadata);
+            return importer.ImportAllEndpoints();
+        }
+
+        public static ServiceEndpointCollection GetEndpoints(string mexAddress)
+        {
+            Uri address = new Uri(mexAddress);
+            ServiceEndpointCollection endpoints = null;
+
+            switch (address.Scheme)
+            {
+                case "net.tcp":
+                    TcpTransportBindingElement tcpBindingElement =
+                        new TcpTransportBindingElement();
+                    tcpBindingElement.MaxReceivedMessageSize *= MessageSizeMultiplier;
+                    endpoints = GetEndpoints(mexAddress, tcpBindingElement);
+                    return endpoints;
+                case "net.pipe":
+                    NamedPipeTransportBindingElement pipeBindingElement =
+                        new NamedPipeTransportBindingElement();
+                    pipeBindingElement.MaxReceivedMessageSize *= MessageSizeMultiplier;
+                    endpoints = GetEndpoints(mexAddress, pipeBindingElement);
+                    return endpoints;
+                default:
+                    return null;
+            }
+        }
+
+        public static bool SupportsContract(
+            string mexAddress,
+            Type contractType) 
+        {
+            if (contractType.IsInterface == false)
+            {
+                throw new ArgumentException(contractType.Name + " is not an interface", "contractType");
+            }
+            object[] attributes = contractType.GetCustomAttributes(typeof(ServiceContractAttribute), false);
+            if (attributes.Length == 0)
+            {
+                throw new ArgumentException("Interface does not have the ServiceContractAttribute", "contractType");
+            }
+            ServiceContractAttribute attribute = attributes[0] as ServiceContractAttribute;
+            if (attribute.Name == null)
+            {
+                attribute.Name = contractType.ToString();
+            }
+            if (attribute.Namespace == null)
+            {
+                attribute.Namespace = "http://tempuri.org/";
+            }
+            return SupportsContract(mexAddress, attribute.Namespace, attribute.Name);
+        }
+
+        public static bool SupportsContract(
+            string mexAddress, 
+            string contractNamespace, 
+            string contractName)
+        {
+            if (string.IsNullOrEmpty(contractNamespace))
+            {
+                throw new ArgumentException("Empty namespace", "contractNamespace");
+            }
+            if (string.IsNullOrEmpty(contractName))
+            {
+                throw new ArgumentException("Empty name", "contractName");
+            }
+            try
+            {
+                ServiceEndpointCollection endpoints = GetEndpoints(mexAddress);
+                foreach (ServiceEndpoint endpoint in endpoints)
+                {
+                    if (endpoint.Contract.Namespace == contractNamespace &&
+                        endpoint.Contract.Name == contractName)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch { }
+            return false;
+        }
+    }
+}
