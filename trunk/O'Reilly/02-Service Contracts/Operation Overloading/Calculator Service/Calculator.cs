397e924ca1f04cb71a9e326f702654a7357704c4
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-06 20:04:27 +0000

    Added 02-Service Contracts

---
+++ trunk/O'Reilly/02-Service Contracts/Operation Overloading/Calculator Service/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace System.ServiceModel.Examples
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract(Name="AddInt")]
+        int Add(int operand1, int operand2);
+
+        [OperationContract(Name="AddDouble")]
+        double Add(double operand1, double operand2);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public double Add(double operand1, double operand2)
+        {
+            return operand1 + operand2;
+        }
+    }
+}
---
+++ trunk/O'Reilly/02-Service Contracts/Operation Overloading/Service Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ServiceModel.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(Calculator), new Uri("http://localhost:8000"));
+            WSHttpBinding binding = new WSHttpBinding();
+            host.AddServiceEndpoint("System.ServiceModel.Examples.ICalculator", binding, "Calculator");
+            host.Open();
+            Console.ReadKey(true);
+            host.Close();
+        }
+    }
+}
