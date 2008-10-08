b6014447eaabf24d61fbe4c508b1e615d67c34c9
mark.alan.good <mark.alan.good@gmail.com>
2008-10-08 13:03:25 +0000

    Added CalculatorClient.cs and app.config, manually (not a service reference). Client side does not work yet.  Not sure why.

---
+++ O'Reilly/02-Service Contracts/Operation Overloading/Calculator Service Client/CalculatorClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.ServiceModel.Examples
+{
+    [ServiceContract]
+    public interface ICalculator
+    {
+        [OperationContract(Name = "AddInt")]
+        int Add(int arg1, int arg2);
+
+        [OperationContract(Name = "AddDouble")]
+        int Add(double arg1, double arg2);
+    }
+
+    class CalculatorClient : ClientBase<ICalculator>, ICalculator
+    {
+
+        #region ICalculator Members
+
+        public int Add(int operand1, int operand2)
+        {
+            return Channel.Add(operand1,operand2);
+        }
+
+        public int Add(double operand1, double operand2)
+        {
+            return Channel.Add(operand1, operand2);
+        }
+
+        #endregion
+    }
+}
