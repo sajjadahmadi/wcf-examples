4574c608c0f07c2851a269f2d4223004d04c0980
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-15 20:04:36 +0000

    Added ChannelFactory and ChannelFactoryTests

---
+++ trunk/System.ServiceModel.Extensions/ExtensionTests/ChannelFactoryTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.ServiceModel.Test
+{
+    /// <summary>
+    /// Summary description for ChannelFactoryTests
+    /// </summary>
+    [TestClass]
+    public class ChannelFactoryTests
+    {
+        public ChannelFactoryTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+
+        [TestMethod]
+        public void CreateInstanceTest()
+        {
+            ITestContract proxy1 = ChannelFactory<TestService, ITestContract>.CreateChannel();
+            Assert.AreEqual<string>("MyResult", proxy1.MyOperation());
+            InProcFactory.CloseProxy<ITestContract>(proxy1);
+        }
+
+        [TestMethod]
+        public void CreateMultipleInstancesTest()
+        {
+            ITestContract proxy1 = ChannelFactory<TestService, ITestContract>.CreateChannel();
+            ITestContract proxy2 = ChannelFactory<TestService, ITestContract>.CreateChannel();
+            Assert.AreNotSame(proxy1, proxy2);
+            Assert.AreEqual<string>("MyResult", proxy1.MyOperation());
+            Assert.AreEqual<string>("MyResult", proxy2.MyOperation());
+        }
+    }
+
+    public class ChannelFacotry<T1, T2, TBinding> : ChannelFactory<T1>
+    {
+    }
+
+}
