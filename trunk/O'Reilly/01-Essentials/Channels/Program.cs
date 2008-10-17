76fef09d0d33849144143a957a67f21f2d18026b
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-17 20:21:32 +0000

    Added Channels example.  This doesn't do much but may add to it in the future.

---
+++ trunk/O'Reilly/01-Essentials/Channels/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels;
+
+namespace Channels
+{
+    [ServiceContract]
+    public interface IMyContract
+    {
+
+        [OperationContract]
+        string MyOperation();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMyContract> factory;
+            factory = new ChannelFactory<IMyContract>("MyEndpoint");
+            IMyContract proxy = factory.CreateChannel();
+            using (proxy as IDisposable)
+            {
+                string result = proxy.MyOperation();
+                Console.WriteLine("MyOperation: {0}", result);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
