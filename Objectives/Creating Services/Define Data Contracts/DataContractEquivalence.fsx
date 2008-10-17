dcce3cdfdae23c38d67b2d3a596b1d8b7cefda3f
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-17 17:36:17 +0000

    Add DataContractEquivalence example.

---
+++ Objectives/Creating Services/Define Data Contracts/DataContractEquivalence.fsx
@@ -0,0 +1,29 @@
+#light
+#r "System.Xml.Linq"
+#r "System.Runtime.Serialization"
+#load "../../ref/Helpers.fsx"
+open System
+open System.Runtime.Serialization
+
+[<DataContract(Name="Person")>]
+type Person =
+    { [<DataMember>]
+      mutable Name : string;
+      
+      [<DataMember>]
+      mutable Age : int }
+
+[<DataContract(Name="Person")>]
+type Employee =
+    { [<DataMember(Name="Name")>]
+      mutable FullName : string;
+      
+      [<DataMember(Name="Age")>]
+      mutable AgeInYears : int }
+
+let person = { Name = "Ray"; Age = 35 }
+let persondata = Helpers.serialize person
+printfn "%s\n\n----------------------\n" persondata
+
+let emp = Helpers.deserialize<Employee> persondata
+printfn "%A" emp
