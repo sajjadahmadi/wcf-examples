c2a74edc7845cacf6ab3a20861857a4440c50376
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-20 16:21:19 +0000

    Renamed O'Reilly to Programming WCF Services

---
+++ Programming WCF Services/02-Service Contracts/Operation Overloading/Calculator Service Client/CalculatorClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace System.ServiceModel.Examples
+{
+    [ServiceContract(ConfigurationName = "ICalculator")]
+    public interface ICalculator
+    {
+        [OperationContract(Name = "AddInt")]
+        int Add(int operand1, int operand2);
+
+        [OperationContract(Name = "AddDouble")]
+        double Add(double operand1, double operand2);
+    }
+
+    class CalculatorClient : ClientBase<ICalculator>, ICalculator
+    {
+        public int Add(int operand1, int operand2)
+        {
+            return Channel.Add(operand1, operand2);
+        }
+
+        public double Add(double operand1, double operand2)
+        {
+            return Channel.Add(operand1, operand2);
+        }
+    }
+}
