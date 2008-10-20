899cebcd446634e321a7e84190341b7fcf8caa36
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-20 16:21:19 +0000

    Renamed O'Reilly to Programming WCF Services

---
+++ trunk/Programming WCF Services/04-Instance Management/Counter Service Library/CounterService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CounterServiceLibrary
+{
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall)]
+    public class PerCallCounter : ICounterService
+    {
+        int currentCount = 0;
+
+        public int IncrementAndReturnCount()
+        {
+            return currentCount += 1;
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class PerSessionCounter : ICounterService
+    {
+        int currentCount = 0;
+
+        public int IncrementAndReturnCount()
+        {
+            return currentCount += 1;
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class SingletonCounter : ICounterService
+    {
+        int currentCount = 0;
+
+        public int IncrementAndReturnCount()
+        {
+            return currentCount += 1;
+        }
+    }
+}
---
+++ trunk/Programming WCF Services/04-Instance Management/Counter Service Library/ICounterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CounterServiceLibrary
+{
+    [ServiceContract]
+    public interface ICounterService
+    {
+        [OperationContract]
+        int IncrementAndReturnCount();
+    }
+}
