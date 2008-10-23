404d6470c37c3624007d0d118ff43d85975dd225
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-23 12:57:25 +0000

    Add data contract examples:   Delegates: Not working.   Enumerations   Generics: Mostly working

---
+++ Objectives/Creating Services/Define Data Contracts/Enumerations.fsx
@@ -0,0 +1,16 @@
+#light
+#r "System.Xml.Linq"
+#r "System.Runtime.Serialization"
+#load "../../ref/Helpers.fsx"
+open System
+open System.Runtime.Serialization
+
+[<DataContract(Name="ContactType")>]
+type ContactType =
+    | [<EnumMember(Value="MyCustomer")>] Customer = 0
+    | [<EnumMember>] Vendor   = 1
+    | Partner  = 2
+      
+let cType = ContactType.Customer
+let data = Helpers.serialize cType
+printfn "%s" data
