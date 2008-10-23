f189e29db906229986f45f670b63cea8c31ed452
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-23 17:23:08 +0000

    Group F# examples in a single folder.

---
+++ InFSharp/OneWayOperationsAndExceptions.fsx
@@ -0,0 +1,38 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#load "../../ref/InProcHost.fsx"
+open Mcts_70_503
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+
+[<ServiceContract>]
+type IMyContract =
+    [<OperationContract(IsOneWay = true)>]
+    abstract MethodWithError : unit -> unit
+    
+    [<OperationContract>]
+    abstract MethodWithoutError : unit -> unit
+
+type MyService() =
+    
+    interface IMyContract with
+        member this.MethodWithError() = failwith "BOOM!"
+        
+        member this.MethodWithoutError() = printfn "MyService.MethodWithoutError()"
+
+let host = new InProcHost<MyService>()
+host.AddEndPoint<IMyContract>()
+host.Open()
+
+let proxy = host.CreateProxy<IMyContract>()
+proxy.MethodWithError()
+try
+    // will error because channel is at fault
+    proxy.MethodWithoutError()    
+with ex -> printfn "\n%s\n" ex.Message
+
+printfn "%A" host.InnerHost.State
+
+host.Close()
