fff70c54604aaeda6dec3bd755ca99dfc4d0b299
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-23 16:28:05 +0000

    Add throttling examples.

---
+++ trunk/Objectives/Exposing and Deploying Services/Create Custom Behaviors/ThrottlingInstances.fsx
@@ -0,0 +1,35 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#load "../../ref/InProcHost.fsx"
+open Mcts_70_503
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+
+[<ServiceContract(SessionMode = SessionMode.Required)>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyMethod : unit -> unit
+
+type MyService() =
+    interface IMyContract with
+        member this.MyMethod() = printfn "%s" "MyService.MyMethod()"
+
+let host = new InProcHost<MyService>()
+host.AddEndPoint<IMyContract>(new NetNamedPipeBinding(SendTimeout = new TimeSpan(0, 0, 5)))
+let throttle = new ServiceThrottlingBehavior(MaxConcurrentInstances = 1)
+host.InnerHost.Description.Behaviors.Add(throttle)
+host.Open()
+
+let proxy1 = host.CreateProxy<IMyContract>()
+proxy1.MyMethod()
+
+let proxy2 = host.CreateProxy<IMyContract>()
+try
+    proxy2.MyMethod()
+    host.CloseProxy(proxy2)
+with ex -> printfn "\n%s\n" ex.Message
+
+host.CloseProxy(proxy1)
+host.Close()
