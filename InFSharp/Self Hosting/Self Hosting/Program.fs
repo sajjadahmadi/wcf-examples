0cdfcddd3c0ad0526f25ead21522502accf16fab
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-24 12:13:05 +0000

    Add F# Self Hosting example.

---
+++ InFSharp/Self Hosting/Self Hosting/Program.fs
@@ -0,0 +1,23 @@
+﻿#light
+open System
+open System.Diagnostics
+open System.ServiceModel
+
+[<ServiceContract>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyOperation : unit -> string
+
+type MyService() =
+    interface IMyContract with
+        member this.MyOperation() = "MyResult"
+
+
+let uris: Uri[] = [| |]
+let host = new ServiceHost(typeof<MyService>, uris)
+host.Open()
+
+let iePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Internet Explorer\\IEXPLORE.EXE")
+Process.Start(iePath, "http://localhost:8000") |> ignore
+Console.ReadKey(true) |> ignore
+host.Close()
