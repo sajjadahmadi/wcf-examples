ec75244d1d698c9f81607028a3c51a713ccd23c7
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-27 14:52:04 +0000

    Added Streaming example.  (Does not work yet.)

---
+++ Examples/Operations/Streaming/CS/Service/StreamService.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceModel;
+using System.IO;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IStreamServiceContract
+    {
+        [OperationContract]
+        Stream EchoStream(Stream stream);
+    }
+    
+    public class StreamService : IStreamServiceContract
+    {
+        #region IStreamServiceContract Members
+
+        public Stream EchoStream(Stream stream)
+        {
+            //stream = new MemoryStream();
+            //StreamWriter writer = new StreamWriter(stream);
+            //writer.WriteLine("This is a test...");
+            //stream.Position = 0;
+
+            return stream;
+        }
+
+        #endregion
+    }
+}
