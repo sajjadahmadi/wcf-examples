002283248cd92268797ac537efd062f7692b2e4b
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-28 20:01:37 +0000

    Added Faults/FaultException

---
+++ Examples/Faults/FaultException/Client/MyClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using Client.MyServiceReference;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Client
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<FaultType>))]
+        public void TypedFaultException()
+        {
+            MyContractClient client = new MyContractClient();
+            client.ThrowTypedFault();
+            client.Close();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException))]
+        public void UntypedFaultException()
+        {
+            MyContractClient client = new MyContractClient();
+            client.ThrowUntypedFault();
+            client.Close();
+        }
+
+    }
+}
---
+++ Examples/Faults/FaultException/Service/MyService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Faults
+{
+    [ServiceContract]
+    public interface IMyContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(FaultType))]
+        void ThrowTypedFault();
+
+        [OperationContract]
+        void ThrowUntypedFault();
+    }
+
+    [DataContract]
+    public class FaultType
+    {
+        private string description;
+
+        public FaultType(string description)
+        { this.description = description; }
+
+        [DataMember]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+
+    public class MyService : IMyContract
+    {
+        public void ThrowTypedFault()
+        {
+            try
+            {
+                throw new ApplicationException("Some application error.");
+            }
+            catch (ApplicationException ex)
+            {
+                FaultType fault = new FaultType(ex.Message);
+                throw new FaultException<FaultType>(fault);
+            }
+        }
+
+        public void ThrowUntypedFault()
+        {
+            throw new FaultException("Untyped Fault.");
+        }
+    }
+
+}
