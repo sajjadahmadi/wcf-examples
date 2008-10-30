522e606146ed9b3439d4f495fd9145a65afeba9f
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-30 16:04:29 +0000

    Add spaces to example file names.

---
+++ trunk/InFSharp/Using ProvideFault.fsx
@@ -0,0 +1,66 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#load "InProcHost.fsx"
+open Mcts_70_503
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+open System.ServiceModel.Dispatcher
+
+[<ServiceContract>]
+type ICalculator =
+    [<OperationContract>]
+    abstract Divide : double * double -> double
+
+type Calculator() =
+    interface ICalculator with
+        member this.Divide(n1, n2) =
+            match n2 with
+            | 0.0  ->
+                raise (new DivideByZeroException())
+            | _    -> n1 / n2
+
+type MyErrorHandler() =
+    interface IErrorHandler with
+        member this.HandleError(error) =
+            printfn "MyErrorHandler.HandleError()"
+            false
+        
+        member this.ProvideFault(error, version, fault) =
+            printfn "MyErrorHandler.ProvideFault(): %A" fault
+
+
+type ErrorServiceBehavior() =
+    interface IServiceBehavior with
+        member this.AddBindingParameters(serviceDescription, serviceHostBase, endpoints, bindingParameters) =
+            ()
+            
+        member this.ApplyDispatchBehavior(serviceDescription, serviceHostBase) =
+            for cd in serviceHostBase.ChannelDispatchers do
+                (cd :?> ChannelDispatcher).ErrorHandlers.Add(new MyErrorHandler())
+
+        member this.Validate(serviceDescription, serviceHostBase) =
+            ()
+
+
+let host = new InProcHost<Calculator>()
+host.AddEndpoint<ICalculator>()
+host.InnerHost.Description.Behaviors.Add(new ErrorServiceBehavior())
+host.Open()
+
+
+let proxy = host.CreateProxy<ICalculator>()
+
+try
+    proxy.Divide(4.0, 0.0) |> ignore
+with ex ->
+    printfn "%A: %s\n\n" (ex.GetType()) ex.Message
+    printfn "Proxy state = %A\n\n" (proxy :?> ICommunicationObject).State
+
+try
+    host.CloseProxy(proxy)
+with _ -> ()
+host.Close()
+
+Threading.Thread.Sleep(100)
