30815a66b769babff98caf74527e53db359126f1
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-10-31 12:50:39 +0000

    Added ErrorHandlingExtensions.cs Added ErrorHandling.cs

---
+++ System.ServiceModel.Examples/Faults/ErrorHandlingExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+using System.ServiceModel;
+using System.Collections.ObjectModel;
+
+class BasicErrorHandlerBehaviorAttribute : Attribute, IErrorHandler, IServiceBehavior
+{
+    internal bool provideFaultCalled;
+
+    #region IErrorHandler Members
+
+    public bool HandleError(Exception error)
+    { return false; }
+
+    public void ProvideFault(Exception error, MessageVersion version, ref Message fault)
+    {
+        provideFaultCalled = true;
+    }
+
+    #endregion
+
+    #region IServiceBehavior Members
+
+    public void AddBindingParameters(ServiceDescription description, ServiceHostBase host, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+    { }
+    public void ApplyDispatchBehavior(ServiceDescription description, ServiceHostBase host)
+    {
+        foreach (ChannelDispatcher dispatcher in host.ChannelDispatchers)
+        { dispatcher.ErrorHandlers.Add(this); }
+    }
+    public void Validate(ServiceDescription description, ServiceHostBase host)
+    { }
+
+    #endregion
+}
