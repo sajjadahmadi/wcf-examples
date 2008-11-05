bf94c43ab77278371210ddb6c34e79989ae0c73a
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-05 16:41:59 +0000

    Added Transactions example (work in progress)

---
+++ trunk/System.ServiceModel.Examples/Transactions/Service.cs
@@ -0,0 +1,52 @@
+﻿using System.ServiceModel.Channels;
+using System.Transactions;
+using System.Diagnostics;
+
+namespace System.ServiceModel.Examples
+{
+    [ServiceContract]
+    interface IClientTxNotAllowed
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.NotAllowed)]
+        void MyMethod();
+    }
+    [ServiceContract]
+    interface IClientTxAllowed
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void MyMethod();
+    }
+    [ServiceContract]
+    interface IClientTxMandatory
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Mandatory)]
+        void MyMethod();
+    }
+
+
+    [BindingRequirementAttribute(TransactionFlowEnabled=true)]
+    class TxService : IClientTxNotAllowed, IClientTxAllowed, IClientTxMandatory
+    {
+        [OperationBehavior(TransactionScopeRequired = true)]
+        void IClientTxNotAllowed.MyMethod()
+        {
+            Debug.Assert(Transaction.Current != null);
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        void IClientTxAllowed.MyMethod()
+        {
+            Debug.Assert(Transaction.Current != null);
+        }
+
+        [OperationBehavior(TransactionScopeRequired = true)]
+        void IClientTxMandatory.MyMethod()
+        {
+            Debug.Assert(Transaction.Current != null);
+        }
+    }
+
+}
