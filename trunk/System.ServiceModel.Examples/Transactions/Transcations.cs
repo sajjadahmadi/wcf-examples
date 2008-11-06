2e7bf3eca0971af6f885c0017804d41dce5eac80
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-06 17:15:25 +0000

    Transcation Flow:  A good example

--- trunk/System.ServiceModel.Examples/Transactions/Transcations.cs
+++ trunk/System.ServiceModel.Examples/Transactions/Transcations.cs
@@ -9,16 +9,26 @@ using System.Diagnostics;
 
 namespace System.ServiceModel.Examples
 {
+    /// <summary>
+    /// TransactionFlow - From the client
+    /// TransactionScopeRequire - Service requires transaction
+    /// Scope   Binding	Flow	Result
+    /// FALSE	FALSE	FALSE	Method executes without a transaction.
+    /// TRUE	FALSE	FALSE	Method creates and executes within a new transaction.
+    ///         FALSE	TRUE	A SOAP fault is returned for the transaction header.
+    /// FALSE	TRUE	TRUE	Method executes without a transaction.
+    /// TRUE	TRUE	TRUE	Method executes under the flowed transaction.
+    /// </summary>
     [TestClass]
     public class Transcations
     {
         static NetNamedPipeBinding txBinding;
         static NetNamedPipeBinding noTxBinding;
 
-        static ServiceHost<TxService> txHost;
-        static string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+        static ServiceHost<MyService> host;
+        static string noFlowAddress = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+        static string txFlowAddress = "net.pipe://localhost/" + Guid.NewGuid().ToString();
 
-        #region Additional test attributes
         // Use ClassInitialize to run code before running the first test in the class
         [ClassInitialize()]
         public static void MyClassInitialize(TestContext testContext)
@@ -30,114 +40,137 @@ namespace System.ServiceModel.Examples
             noTxBinding = new NetNamedPipeBinding();
             noTxBinding.TransactionFlow = false;
 
-            txHost = new ServiceHost<TxService>();
-            txHost.AddServiceEndpoint<IServiceTxContract>(txBinding, address);
-            txHost.IncludeExceptionDetailInFaults = true;
-            txHost.Open();
+            host = new ServiceHost<MyService>();
+            host.AddServiceEndpoint<ITxFlow>(txBinding, txFlowAddress);
+            host.AddServiceEndpoint<INoFlow>(txBinding, noFlowAddress);
+            host.IncludeExceptionDetailInFaults = true;
+            host.Open();
         }
+
         [ClassCleanup()]
         public static void MyClassCleanup()
-        { txHost.Close(); }
-        #endregion
-
-
-        class ServiceTxClient : ClientBase<IServiceTxContract>, IServiceTxContract
         {
-            public ServiceTxClient(Binding binding, string remoteAddress)
-                : base(binding, new EndpointAddress(remoteAddress)) { }
-
-            #region IServiceTxContract Members
-
-            public TransactionInfo ClientTxNotAllowedMethod()
-            { return Channel.ClientTxNotAllowedMethod(); }
-
-            public TransactionInfo ClientTxAllowedMethod()
-            { return Channel.ClientTxAllowedMethod(); }
-
-            #endregion
-
-        }
-
-        class ClientTxClient : ClientBase<IClientTxContract>, IClientTxContract
-        {
-            public ClientTxClient(Binding binding, string remoteAddress)
-                : base(binding, new EndpointAddress(remoteAddress)) { }
-
-            #region IClientTxContract Members
-
-            public TransactionInfo ClientTxAllowedMethod()
-            { return Channel.ClientTxAllowedMethod(); }
-
-            public TransactionInfo ClientTxMandatoryMethod()
-            { return Channel.ClientTxMandatoryMethod(); }
-
-            #endregion
-
+            host.Close();
         }
 
 
-
+        /// <summary>
+        /// ScopeNotRequired() 
+        /// Transaction Mode:  NONE
+        /// </summary>
         [TestMethod]
-        // Service-Only Mode:
-            //
-        public void ServiceOnlyTranscationMode()
+        public void ScopeNotRequired()
         {
-            ServiceTxClient proxy = new ServiceTxClient(noTxBinding, address);
+            NoFlowClient proxy = new NoFlowClient(noTxBinding, noFlowAddress);
             proxy.Open();
             using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
             {
-                TransactionInfo info = proxy.ClientTxNotAllowedMethod();
-                Assert.IsTrue(info.HasTransaction);
-                Assert.IsTrue(info.DistributedIdentifier == Guid.Empty);
-                Assert.IsNotNull(info.LocalIdentifier);
-            }
-            using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
-            {
-                TransactionInfo info = proxy.ClientTxAllowedMethod();
-                Assert.IsTrue(info.HasTransaction);
-                Assert.IsTrue(info.DistributedIdentifier == Guid.Empty);
-                Assert.IsNotNull(info.LocalIdentifier);
+                TransactionInfo info = proxy.ScopeNotRequired();
+                Assert.IsFalse(info.HasAmbientTransaction, "Expected no ambient transaction.");
             }
             proxy.Close();
         }
 
         /// <summary>
-        /// Client/Serivce Mode: 
-        /// Use the client-side transaction if one exists, 
-        /// otherwise use a service-side transaction
+        /// ScopeRequired_FlowUnspecified() 
+        /// Transaction Mode: Service
+        /// Client's transaction is ignored.
+        /// Service creates new transaction.
+        /// Same as Flow NotAllowed
         /// </summary>
         [TestMethod]
-        public void ClientServiceMode()
+        public void ScopeRequired_FlowUnspecified()
         {
-            ServiceTxClient proxy = new ServiceTxClient(txBinding, address);
+            NoFlowClient proxy = new NoFlowClient(noTxBinding, noFlowAddress);
             proxy.Open();
             using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
             {
-                // Service-side transaction
-                TransactionInfo info = proxy.ClientTxNotAllowedMethod();
-                Assert.IsTrue(info.HasTransaction);
-                Assert.IsTrue(info.DistributedIdentifier == Guid.Empty);
-                Assert.IsFalse(info.ClientSideTransaction);
-                Assert.IsNotNull(info.LocalIdentifier);
+                TransactionInfo info = proxy.FlowUnspecified();
+                Assert.IsTrue(info.UsingServiceSideTransaction, "Expected service-side transaction");
+                Assert.IsFalse(info.UsingClientSideTransaction);
             }
+            proxy.Close();
+        }
+
+        /// <summary>
+        /// ScopeRequired_FlowNotAllowed()
+        /// Transaction Mode: Service
+        /// Client's transaction is ignored.
+        /// Service creates new transaction.
+        /// Same as Flow NotSpecified
+        /// </summary>
+        [TestMethod]
+        public void ScopeRequired_FlowNotAllowed()
+        {
+            NoFlowClient proxy = new NoFlowClient(noTxBinding, noFlowAddress);
+            proxy.Open();
             using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
             {
-                // Client-side transaction
-                TransactionInfo info = proxy.ClientTxAllowedMethod();
-                Assert.IsTrue(info.HasTransaction);
-                Assert.IsTrue(info.DistributedIdentifier != Guid.Empty);
-                Assert.IsTrue(info.ClientSideTransaction);
-                Assert.IsNotNull(info.LocalIdentifier);
+                TransactionInfo info = proxy.FlowNotAllowed();
+                Assert.IsTrue(info.UsingServiceSideTransaction, "Expected service-side transaction");
+                Assert.IsFalse(info.UsingClientSideTransaction);
             }
-            // Client-side transaction is allowed here, but we didn't create one...
+            proxy.Close();
+        }
+
+        /// <summary>
+        /// ScopeRequired_FlowAllowed
+        /// Transaction Mode: Client/Serivce 
+        /// Uses the client's transaction if one exists, 
+        /// otherwise service creates a new transaction
+        /// </summary>
+        [TestMethod]
+        public void ScopeRequired_FlowAllowed()
+        {
+            TxFlowClient proxy = new TxFlowClient(txBinding, txFlowAddress);
+            proxy.Open();
+            using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
             {
-                // Service-side transaction
-                TransactionInfo info = proxy.ClientTxAllowedMethod();
-                Assert.IsTrue(info.HasTransaction);
-                Assert.IsTrue(info.DistributedIdentifier == Guid.Empty);
-                Assert.IsFalse(info.ClientSideTransaction);
-                Assert.IsNotNull(info.LocalIdentifier);
+                TransactionInfo info = proxy.FlowAllowed();
+                Assert.IsFalse(info.UsingServiceSideTransaction, "Expected client-side transaction.");
+                Assert.IsTrue(info.UsingClientSideTransaction);
             }
+            {
+                TransactionInfo info = proxy.FlowAllowed();
+                Assert.IsTrue(info.UsingServiceSideTransaction, "Expected service-side transaction.");
+                Assert.IsFalse(info.UsingClientSideTransaction);
+            }
+            proxy.Close();
+        }
+
+        /// <summary>
+        /// ScopeRequired_FlowMandatory
+        /// Transaction Mode: Client 
+        /// Requires the client to flow a transaction.
+        /// </summary>
+        [TestMethod]
+        public void ScopeRequired_FlowMandatory()
+        {
+            TxFlowClient proxy = new TxFlowClient(txBinding, txFlowAddress);
+            proxy.Open();
+            using (TransactionScope tx = new TransactionScope(TransactionScopeOption.RequiresNew))
+            {
+                TransactionInfo info = proxy.FlowMandatory();
+                Assert.IsTrue(info.UsingClientSideTransaction, "Expected client-side transaction.");
+                Assert.IsFalse(info.UsingServiceSideTransaction);
+            }
+            proxy.Close();
+        }
+
+        /// <summary>
+        /// ScopeRequired_FlowMandatory_ProtocolException
+        /// Transaction Mode: Client 
+        /// Requires the client to flow a transaction.
+        /// Throws ProtocolException when transaction is not flowed.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ProtocolException), 
+            "The service operation requires a transaction to be flowed.")]
+        public void ScopeRequired_FlowMandatory_ProtocolException()
+        {
+            TxFlowClient proxy = new TxFlowClient(txBinding, txFlowAddress);
+            proxy.Open();
+            TransactionInfo info = proxy.FlowMandatory();
             proxy.Close();
         }
     }
