30c5df7132f5e80e74f42bc3f8ff516ff7609420
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-11 19:41:19 +0000

    Add transaction voting example.

---
+++ InFSharp/Transaction Voting.fsx
@@ -0,0 +1,62 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#r "System.Transactions"
+#load "InProcHost.fsx"
+open System
+open System.ServiceModel
+open System.Transactions
+open System.Diagnostics
+open Mcts_70_503
+
+
+[<ServiceContract(SessionMode = SessionMode.Required)>]
+type IMyContract =
+    [<OperationContract>]
+    [<TransactionFlow(TransactionFlowOption.Allowed)>]
+    abstract MyMethod : unit -> unit
+    
+    [<OperationContract>]
+    [<TransactionFlow(TransactionFlowOption.Allowed)>]
+    abstract MyOtherMethod : unit -> unit
+        
+
+type MyService() =
+    interface IMyContract with
+        // DECLARATIVE VOTING
+        [<OperationBehavior(TransactionScopeRequired = true, TransactionAutoComplete = true)>]
+        // It is very important when TransactionScopeRequired = true to avoid
+        //   catching and handling exceptions and explicitly voting to abort
+        // If you want to catch the exception for some local handling, make
+        //   sure to rethrow it
+        member this.MyMethod() =
+            try
+                use scope = new TransactionScope()
+                // some work
+                scope.Complete()
+            with ex -> raise ex 
+
+        // EXPLICIT VOTING
+        [<OperationBehavior(TransactionScopeRequired = true, TransactionAutoComplete = false)>]
+        member this.MyOtherMethod() =
+            try
+                use scope = new TransactionScope()
+                // some work
+                OperationContext.Current.SetTransactionComplete()
+            with ex -> raise ex
+
+
+let host = new InProcHost<MyService>()
+let binding = new NetNamedPipeBinding(TransactionFlow = true)
+host.AddEndpoint<IMyContract>(binding)
+host.IncludeExceptionDetailInFaults <- true
+host.Open()
+let proxy = host.CreateProxy<IMyContract>()
+
+let scope = new TransactionScope()
+proxy.MyMethod()
+proxy.MyOtherMethod()
+scope.Complete()
+
+host.CloseProxy(proxy)
+host.Close()
