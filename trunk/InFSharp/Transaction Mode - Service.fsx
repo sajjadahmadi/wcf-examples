8b8b3a35bf337cecce2a6916bb9d9f5c6af22eb6
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-11 02:52:46 +0000

    Add Service transaction mode example.

---
+++ trunk/InFSharp/Transaction Mode - Service.fsx
@@ -0,0 +1,65 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#r "System.Transactions"
+#load "InProcHost.fsx"
+open System
+open System.ServiceModel
+open System.Transactions
+open System.Diagnostics
+open Mcts_70_503
+
+
+let printTrans() =
+    let ambTrans = Transaction.Current
+    if ambTrans <> null then
+        let info = ambTrans.TransactionInformation
+        printfn "Dist  ID %A\nLocal ID %s" info.DistributedIdentifier info.LocalIdentifier
+    else
+        printfn "No Transaction"
+            
+            
+[<ServiceContract>]
+type IMyContract =
+    [<OperationContract>]
+    [<TransactionFlow(TransactionFlowOption.NotAllowed)>]
+    abstract MyMethod : unit -> unit
+        
+
+type MyService() =
+    interface IMyContract with
+        [<OperationBehavior(TransactionScopeRequired = true)>]
+        member this.MyMethod() =
+            use scope = new TransactionScope()
+            printTrans()
+            scope.Complete()
+
+// The Service mode ensures that the service always has a transaction
+//   separate from any transaction its clients may or may not have
+
+let host = new InProcHost<MyService>()
+let binding = new NetNamedPipeBinding(TransactionFlow = false)
+host.AddEndpoint<IMyContract>(binding)
+host.IncludeExceptionDetailInFaults <- true
+host.Open()
+let proxy = host.CreateProxy<IMyContract>()
+
+printfn "No Scope"
+proxy.MyMethod()
+printfn "---------------------"
+
+let scope = new TransactionScope()
+
+printfn "Local Scope"
+printTrans()
+printfn "---------------------"
+
+printfn "Service Scope"
+proxy.MyMethod()
+printfn "---------------------"
+
+scope.Complete()
+
+
+host.CloseProxy(proxy)
+host.Close()
