f654b8267d6813a327bffaf397ccac0fb76b564e
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-24 14:59:47 +0000

    Moved InProcFactory and ServiceHost to Hosting folder. Added FormHost

---
+++ CodeRunner/ServiceModel.Extensions/Hosting/FormHost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace CodeRunner.ServiceModel.Hosting
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public abstract class FormHost<F> : Form
+        where F : Form
+    {
+        ServiceHost<F> host;
+
+        protected ServiceHost<F> Host
+        {
+            get { return host; }
+            set { host = value; }
+        }
+
+        public FormHost(params string[] baseAddresses)
+        {
+            host = new ServiceHost<F>(this as F, baseAddresses);
+
+            Load += delegate
+                {
+                    if (Host.State == CommunicationState.Created)
+                    { Host.Open(); }
+                };
+            FormClosed += delegate
+            {
+                if (Host.State == CommunicationState.Opened)
+                {
+                    Host.Close();
+                }
+            };
+        }
+
+    }
+}
