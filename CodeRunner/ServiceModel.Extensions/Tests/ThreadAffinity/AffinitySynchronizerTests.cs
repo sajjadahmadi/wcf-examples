ddd6aae929d3dfec67c40f570db35dcaf96382f7
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-11-25 21:35:56 +0000

    Added AffinitySynchronizer (similar to Juval's) Added AffinitySynchronizerTests

---
+++ CodeRunner/ServiceModel.Extensions/Tests/ThreadAffinity/AffinitySynchronizerTests.cs
@@ -0,0 +1,100 @@
+﻿using CodeRunner.ServiceModel.ThreadAffinity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace CodeRunner.ServiceModel.ThreadAffinity.Test
+{
+    /// <summary>
+    /// This is a test class for AffinitySynchronizer and is intended
+    /// to contain all AffinitySynchronizer Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AffinitySynchronizerTests
+    {
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+        [ServiceContract]
+        interface IMyContract
+        {
+            [OperationContract]
+            string GetThreadName();
+        }
+
+        [ServiceBehavior(UseSynchronizationContext = true)]
+        class MyService : IMyContract
+        {
+            public string GetThreadName()
+            {
+                return Thread.CurrentThread.Name;
+            }
+        }
+
+        /// <summary>
+        /// Baseline understanding
+        /// </summary>
+        [TestMethod]
+        public void WithoutAffinitySynchronizer()
+        {
+            Assert.IsNull(SynchronizationContext.Current);
+            using (ServiceHost<MyService> host = new ServiceHost<MyService>())
+            {
+                Binding binding = new NetNamedPipeBinding();
+                string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+                host.AddServiceEndpoint<IMyContract>(binding, address);
+                host.Open();
+                IMyContract channel = host.CreateChannel<IMyContract>(binding, address);
+                Assert.IsNull(channel.GetThreadName());
+                InProcFactory.CloseChannel<IMyContract>(channel);
+            }
+        }
+
+        /// <summary>
+        /// Basic test for "installing" AffinitySynchronizer
+        /// </summary>
+        [TestMethod]
+        public void WithAffinitySynchronizer()
+        {
+            SynchronizationContext context = new AffinitySynchronizer("Test Thread");
+            SynchronizationContext.SetSynchronizationContext(context);
+            Assert.IsNotNull(SynchronizationContext.Current);
+
+            using (context as IDisposable)
+            {
+                IMyContract channel = InProcFactory.CreateChannel<MyService, IMyContract>();
+                Assert.AreEqual("Test Thread", channel.GetThreadName());
+                InProcFactory.CloseChannel<IMyContract>(channel);
+            }
+        }
+    }
+}
