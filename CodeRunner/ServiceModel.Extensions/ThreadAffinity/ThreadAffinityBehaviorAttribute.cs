2cb5acc8e83fde521322e39d8acea330f8a6ba4c
mark.alan.good <mark.alan.good@gmail.com>
2008-11-28 13:01:04 +0000

    Completed ThreadAffinityBehaviorAttribute

--- CodeRunner/ServiceModel.Extensions/ThreadAffinity/ThreadAffinityBehaviorAttribute.cs
+++ CodeRunner/ServiceModel.Extensions/ThreadAffinity/ThreadAffinityBehaviorAttribute.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ServiceModel.Description;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
 
 namespace CodeRunner.ServiceModel.ThreadAffinity
 {
     [AttributeUsage(AttributeTargets.Class)]
     public class ThreadAffinityBehaviorAttribute :
         Attribute,
-        IContractBehavior
+        IContractBehavior,
+        IServiceBehavior
     {
         Type m_SeriviceType;
         string m_ThreadName;
 
         public ThreadAffinityBehaviorAttribute(Type serviceType)
-            : this(serviceType, null)
-        { }
+            : this(serviceType, null) { }
         public ThreadAffinityBehaviorAttribute(Type serviceType, string threadName)
         {
             m_SeriviceType = serviceType;
-            m_ThreadName = threadName;
+            m_ThreadName = threadName ??
+                string.Format("Executing endpoints of {0}", m_SeriviceType);
         }
 
         public string ThreadName
@@ -30,21 +34,26 @@ namespace CodeRunner.ServiceModel.ThreadAffinity
         }
 
         #region IContractBehavior
-        void IContractBehavior.AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
-        { }
-
-        void IContractBehavior.ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.ClientRuntime clientRuntime)
-        { }
-
-        void IContractBehavior.ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, System.ServiceModel.Dispatcher.DispatchRuntime dispatchRuntime)
+        void IContractBehavior.AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+        void IContractBehavior.ApplyClientBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, ClientRuntime clientRuntime) { }
+        void IContractBehavior.ApplyDispatchBehavior(ContractDescription contractDescription, ServiceEndpoint endpoint, DispatchRuntime dispatchRuntime)
         {
-            string name = m_ThreadName ?? 
-                string.Format("Executing endpoints of {0}, {1}", m_SeriviceType, contractDescription.ContractType);
-            ThreadAffinityHelper.ApplyDispatchBehavior(m_SeriviceType, name, dispatchRuntime);
+            ThreadAffinityHelper.ApplyDispatchBehavior(m_SeriviceType, m_ThreadName, dispatchRuntime);
         }
+        void IContractBehavior.Validate(ContractDescription contractDescription, ServiceEndpoint endpoint) { }
+        #endregion
 
-        void IContractBehavior.Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
-        { }
+        #region IServiceBehavior Members
+        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters) { }
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase) { }
+        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        {
+            // Shut down the worker thread when the host is closed
+            serviceHostBase.Closed += delegate
+            {
+                ThreadAffinityHelper.CloseThread(m_SeriviceType);
+            };
+        }
         #endregion
     }
 }
