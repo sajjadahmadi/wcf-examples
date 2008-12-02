3593eb0cfce00be9af6bc57c3b0e4ff7d044e6d0
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-02 19:13:33 +0000

    Moved ThreadAffinityBehaviorAttributeTests Added CallbackThreadAffinityBehaviorAttribute.cs (Note: CallbackThreadAffinityBehaviorAttributeTest does not pass yet.)

---
+++ CodeRunner/ServiceModel.Extensions/ThreadAffinity/CallbackThreadAffinityBehaviorAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+
+namespace CodeRunner.ServiceModel.ThreadAffinity
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CallbackThreadAffinityBehaviorAttribute : Attribute, IEndpointBehavior
+    {
+        string m_ThreadName;
+        Type m_callbackType;
+
+        public CallbackThreadAffinityBehaviorAttribute(Type callbackType)
+            : this(callbackType, null) { }
+        public CallbackThreadAffinityBehaviorAttribute(Type callbackType, string threadName)
+        {
+            m_ThreadName = threadName;
+            m_callbackType = callbackType;
+            AppDomain.CurrentDomain.ProcessExit += delegate
+            {
+                ThreadAffinityHelper.CloseThread(m_callbackType);
+            };
+        }
+
+        public string ThreadName { get; set; }
+
+        #region IEndpointBehavior Members
+        void IEndpointBehavior.AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters) { }
+        void IEndpointBehavior.ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime) 
+        {
+            m_ThreadName = m_ThreadName ?? "Executing callbacks of " + m_callbackType;
+            ThreadAffinityHelper.ApplyDispatchBehavior(m_callbackType, m_ThreadName, clientRuntime.CallbackDispatchRuntime);
+        }
+        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher) { }
+        void IEndpointBehavior.Validate(ServiceEndpoint endpoint) { }
+        #endregion
+    }
+}
