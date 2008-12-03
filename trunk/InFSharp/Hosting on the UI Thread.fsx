ee9abeb995e5bae0ec1345743411fcc82efa2c35
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-03 19:35:53 +0000

    Add Hosting on the UI Thread example.

---
+++ trunk/InFSharp/Hosting on the UI Thread.fsx
@@ -0,0 +1,31 @@
+#light
+#r "System.ServiceModel"
+#load "InProcHost.fsx"
+open System
+open System.Windows.Forms
+open System.ServiceModel
+open Mcts_70_503
+
+
+[<ServiceContract>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyMethod : unit -> unit
+
+
+type MyService() =
+    interface IMyContract with
+        member this.MyMethod() = printfn "MyService.MyMethod()"
+
+
+type HostForm() as this =
+    inherit Form()
+    
+    let host = new InProcHost<MyService>()
+    do host.AddEndpoint<IMyContract>()
+    do this.FormClosed.Add(fun e ->
+        host.Close())
+    do host.Open()
+
+
+Application.Run(new HostForm())
