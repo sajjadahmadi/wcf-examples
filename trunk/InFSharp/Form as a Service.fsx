71afff4682e13ab45fde8fa138e382bd636b46ec
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-04 03:38:37 +0000

    Add Form as a Service example.

---
+++ trunk/InFSharp/Form as a Service.fsx
@@ -0,0 +1,44 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+#load "InProcHost.fsx"
+open System
+open System.Windows.Forms
+open System.ServiceModel
+open Mcts_70_503
+
+
+[<ServiceContract>]
+type IFormManager =
+    [<OperationContract>]
+    abstract IncrementLabel : unit -> unit
+
+
+[<ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)>]
+type MyForm() as this =
+    inherit Form()
+    
+    let mutable host : InProcHost<MyForm> option = None
+    let label = new Label(Text="0")
+    do this.Controls.Add(label)
+    do this.Initialize()
+    
+    member private this.Initialize() =
+        let h = new InProcHost<MyForm>(this)
+        h.AddEndpoint<IFormManager>()
+        this.FormClosed.Add(fun e -> h.Close())
+        h.Open()
+        host <- Some h
+    
+    member this.Host = Option.get host
+    
+    member this.IncrementLabel() =
+        label.Text <- string (int label.Text + 1)    
+        
+    interface IFormManager with
+        member this.IncrementLabel() = this.IncrementLabel()
+
+
+let form = new MyForm()
+let proxy = form.Host.CreateProxy<IFormManager>()
+form.ShowDialog()
