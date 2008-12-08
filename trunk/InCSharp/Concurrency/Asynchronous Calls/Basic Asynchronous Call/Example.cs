bbc959300dffcb39f1c3e35f24122f5eab015727
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-08 21:20:02 +0000

    Added Basic Asynchronous Call example

---
+++ trunk/InCSharp/Concurrency/Asynchronous Calls/Basic Asynchronous Call/Example.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Diagnostics;
+using System.ServiceModel.Channels;
+
+namespace CodeRunner.Service
+{
+    [ServiceContract]
+    interface ICalculator
+    {
+        [OperationContract]
+        int Add(int number1, int number2);
+    }
+    class Calculator : ICalculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
+
+namespace CodeRunner.Client
+{
+    [ServiceContract]
+    interface ICalculator
+    {
+        [OperationContract]
+        int Add(int number1, int number2);
+
+        [OperationContract(AsyncPattern = true)]
+        IAsyncResult BeginAdd(int number1, int number2, AsyncCallback callback, object asyncState);
+
+        int EndAdd(IAsyncResult result);
+    }
+
+    class CalculatorClient : ClientBase<ICalculator>, ICalculator
+    {
+        public CalculatorClient(Binding binding, string address)
+            : base(binding, new EndpointAddress(address))
+        { }
+
+        public int Add(int number1, int number2)
+        { return Channel.Add(number1, number2); }
+
+        public IAsyncResult BeginAdd(int number1, int number2, AsyncCallback callback, object asyncState)
+        { return Channel.BeginAdd(number1, number2, callback, asyncState); }
+
+        public int EndAdd(IAsyncResult result)
+        { return Channel.EndAdd(result); }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost<Service.Calculator> host;
+            string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+            using (host = new ServiceHost<CodeRunner.Service.Calculator>())
+            {
+                host.AddServiceEndpoint<Service.ICalculator>(new NetNamedPipeBinding(), address);
+                host.Open();
+
+                CalculatorClient proxy = new CalculatorClient(new NetNamedPipeBinding(), address);
+
+                IAsyncResult result1 = proxy.BeginAdd(2, 3, null, null);
+                IAsyncResult result2 = proxy.BeginAdd(4, 5, null, null);
+
+                /* Do some work */
+
+                int sum;
+
+                sum = proxy.EndAdd(result1);  // This may block
+                Debug.Assert(sum == 5);
+
+                sum = proxy.EndAdd(result2);  // This may block
+                Debug.Assert(sum == 9);
+
+                (proxy as ICommunicationObject).Close();
+            }
+        }
+    }
+}
+
