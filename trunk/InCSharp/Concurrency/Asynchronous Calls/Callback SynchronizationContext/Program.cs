14eb60a6ce9ef7ec1ade30d7c3dc74321654e1a0
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-09 19:12:54 +0000

    Added Callback SynchronizationContext example

---
+++ trunk/InCSharp/Concurrency/Asynchronous Calls/Callback SynchronizationContext/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceModel;
+using System.Threading;
+using System.Windows.Forms;
+using CodeRunner.Client;
+
+namespace CodeRunner
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CounterForm());
+        }
+    }
+}
