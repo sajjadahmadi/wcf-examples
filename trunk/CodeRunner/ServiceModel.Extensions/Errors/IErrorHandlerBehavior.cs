f26a653c22d5d403907480ebef3a7c0877e4a4a9
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-15 21:16:28 +0000

    Refactored (simplified) Fault tests.
    Moved ErrorHandlingExtensions.cs to (and replaced) ServiceModel.Extensions/Errors/ErrorHandlerBehaviorAttribute.cs
    WARNING:  Need to test ErrorHandlerBehaviorAttribute.  Not sure if this will work.

---
+++ trunk/CodeRunner/ServiceModel.Extensions/Errors/IErrorHandlerBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Description;
+
+namespace System.ServiceModel.Errors
+{
+    public interface IErrorHandlerBehavior : IErrorHandler, IServiceBehavior
+    {
+        Type ServiceType { get; }
+    }
+}
