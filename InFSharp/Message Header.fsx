a131f7e7f2f218677a1f40af5d335bbd6118764a
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-16 01:57:41 +0000

    Remove unneeded content.

--- InFSharp/Message Header.fsx
+++ InFSharp/Message Header.fsx
@@ -19,10 +19,8 @@ type IMyContract =
     abstract MyMethod : unit -> unit
 
 
-[<ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)>]
 type MyService() =
     interface IMyContract with
-        [<OperationBehavior(TransactionScopeRequired = true)>]
         member this.MyMethod() =
             let hdrs = OperationContext.Current.IncomingMessageHeaders
             printfn "\nIncomingHeaders\n-----------------"
@@ -30,11 +28,9 @@ type MyService() =
                 printfn "%s: %A\n" h.Name h
 
 
-let binding = new NetNamedPipeBinding()
-binding.TransactionFlow <- true
 let host = new InProcHost<MyService>()
 host.IncludeExceptionDetailInFaults <- true
-host.AddEndpoint<IMyContract>(binding)
+host.AddEndpoint<IMyContract>()
 host.Open()
 
 let factory = new ChannelFactory<IMyContract>(new NetNamedPipeBinding())
@@ -42,9 +38,8 @@ let proxy = factory.CreateChannel(new EndpointAddress("net.pipe://localhost"))
 let headerData = { Member1 = "value" }
 let header = new MessageHeader<MyCustomType>(headerData)
 
-using (new OperationContextScope(proxy :?> IContextChannel)) (fun scope ->
-    OperationContext.Current.OutgoingMessageHeaders.Add(
-        header.GetUntypedHeader("MyCustomType", "ServiceModelEx"))
-    proxy.MyMethod()
-    Threading.Thread.Sleep(100))
+let scope = new OperationContextScope(proxy :?> IContextChannel)
+OperationContext.Current.OutgoingMessageHeaders.Add(
+    header.GetUntypedHeader("MyCustomType", "ServiceModelEx"))
+proxy.MyMethod()
 
