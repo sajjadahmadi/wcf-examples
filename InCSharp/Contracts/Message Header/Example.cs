81a4d5cce976f49ef4bbd48cdc96c753f8fd9967
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-16 21:04:37 +0000

    Added Message Header example

---
+++ InCSharp/Contracts/Message Header/Example.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+
+namespace CodeRunner
+{
+    class Example
+    {
+        static string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+        // Contracts
+        [DataContract]
+        class MyCustomType
+        {
+            [DataMember]
+            public string MyMember;
+        }
+
+        [ServiceContract]
+        interface IMyContract
+        {
+            [OperationContract]
+            void MyMethod();
+        }
+
+        // Service
+        [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+        class MyService : IMyContract
+        {
+            [OperationBehavior(TransactionScopeRequired = true)]
+            public void MyMethod()
+            {
+                MyCustomType headerData =
+                    OperationContext.Current.IncomingMessageHeaders.GetHeader<MyCustomType>("MyCustomType", "CodeRunner");
+                Debug.Assert(headerData.MyMember == "MyCustomType Header Data");
+                return;
+            }
+        }
+
+        // Client
+        class MyContractClient : ClientBase<IMyContract>, IMyContract
+        {
+            public MyContractClient(string address)
+                : base(new NetNamedPipeBinding(), new EndpointAddress(address))
+            { }
+
+            public void MyMethod()
+            {
+                MyCustomType headerData = new MyCustomType() { MyMember = "MyCustomType Header Data" };
+                MessageHeader<MyCustomType> customHeader = new MessageHeader<MyCustomType>(headerData);
+
+                using (OperationContextScope scope =
+                    new OperationContextScope(InnerChannel))
+                {
+                    OperationContext.Current.OutgoingMessageHeaders.Add(
+                        customHeader.GetUntypedHeader("MyCustomType", "CodeRunner"));
+                    Channel.MyMethod();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(MyService), new Uri(address)))
+            {
+                host.AddServiceEndpoint(typeof(IMyContract), new NetNamedPipeBinding(), address);
+                host.Open();
+
+                MyContractClient proxy = new MyContractClient(address);
+                proxy.Open();
+                proxy.MyMethod();
+
+                proxy.Close();
+                host.Close();
+            }
+        }
+    }
+}
