12e9550adc8b77d80674d1ca93d1ba777efd8ae8
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-17 20:56:38 +0000

    Added Security examples: Authentication/Alternative Credentials.cs Impersonation/Declarative Impersonation.cs Impersonation/Impersonation on All Methods.cs Impersonation/Manual Impersonation.cs

---
+++ trunk/InCSharp/Security/Security/Authentication/Alternative Credentials.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+
+namespace CodeRunner.Security
+{
+    [TestClass]
+    public class AlternativeCredentials
+    {
+        // Contracts
+        [ServiceContract]
+        interface IMyContract
+        {
+            [OperationContract]
+            void MyMethod();
+        }
+
+        // Service
+        [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+        class MyService : IMyContract
+        {
+            public void MyMethod()
+            {
+                return;
+            }
+        }
+
+        // Client
+        class MyContractClient : ClientBase<IMyContract>, IMyContract
+        {
+            public MyContractClient() { }
+            public MyContractClient(Binding binding, string address) :
+                base(binding, new EndpointAddress(address)) { }
+
+            public void MyMethod()
+            { Channel.MyMethod(); }
+        }
+
+        #region Host
+        static string address = "net.tcp://localhost:8001/" + Guid.NewGuid().ToString();
+        static ServiceHost host;
+
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            host = new ServiceHost(typeof(MyService));
+            host.AddServiceEndpoint(typeof(IMyContract), new NetTcpBinding(), address);
+            host.Open();
+        }
+
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            if (host.State == CommunicationState.Opened)
+                host.Close();
+        }
+        #endregion
+
+        [TestMethod]
+        public void UseAlternativeCredentials()
+        {
+            using (MyContractClient proxy =
+                new MyContractClient(new NetTcpBinding(), address))
+            {
+                NetworkCredential credentials = new NetworkCredential();
+                credentials.Domain = "MyDomain";
+                credentials.UserName = "MyUsername";
+                credentials.Password = "MyPassword";
+                //proxy.ClientCredentials.Windows.ClientCredential = credentials;
+
+                proxy.Open();
+                proxy.MyMethod();
+                proxy.Close();
+            }
+        }
+    }
+}
---
+++ trunk/InCSharp/Security/Security/Impersonation/Manual Impersonation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace CodeRunner.Security
+{
+    [TestClass]
+    public class ImpersonationAll
+    {
+        // Contracts
+        [ServiceContract]
+        interface IMyContract
+        {
+            [OperationContract]
+            void MyMethod();
+        }
+
+        // Service
+        [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+        class MyService : IMyContract
+        {
+            // Impersonation occures here               
+            [OperationBehavior(Impersonation = ImpersonationOption.Allowed)]
+            public void MyMethod()
+            {
+            }
+        }
+
+        // Client
+        class MyContractClient : ClientBase<IMyContract>, IMyContract
+        {
+            public MyContractClient() { }
+            public MyContractClient(Binding binding, string address) :
+                base(binding, new EndpointAddress(address)) { }
+
+            public void MyMethod()
+            { Channel.MyMethod(); }
+        }
+
+        [TestMethod]
+        public void VerifyImpersonationOnAllOperations()
+        {
+            string address = "net.tcp://localhost:8002/" + Guid.NewGuid().ToString();
+            using (ServiceHost host = new ServiceHost(typeof(MyService)))
+            {
+                host.AddServiceEndpoint(typeof(IMyContract), new NetTcpBinding(), address);
+                // Impersonation VERIFIED here               
+                host.Authorization.ImpersonateCallerForAllOperations = true;
+                host.Open();
+
+                using (MyContractClient proxy =
+                    new MyContractClient(new NetTcpBinding(), address))
+                {
+                    proxy.Open();
+                    proxy.MyMethod();
+                    proxy.Close();
+                }
+
+                host.Close();
+            }
+        }
+    }
+}
