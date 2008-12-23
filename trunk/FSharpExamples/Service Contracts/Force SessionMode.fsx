7d127e3d364efb3a8665cf320a60d898eb7a1211
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-23 19:19:23 +0000

    More example relocates.

---
+++ trunk/FSharpExamples/Service Contracts/Force SessionMode.fsx
@@ -0,0 +1,43 @@
+#light
+#r "System.ServiceModel"
+#r "System.Runtime.Serialization"
+open System
+open System.Diagnostics
+open System.ServiceModel
+open System.ServiceModel.Channels
+
+
+[<ServiceContract(SessionMode = SessionMode.Required)>]
+type IMyContract =
+    [<OperationContract>]
+    abstract MyMethod : unit -> unit
+
+
+type MyService() =
+    let mutable counter = 0
+    
+    do printfn "MyService.MyService()"
+    
+    interface IMyContract with
+        member this.MyMethod() =
+            counter <- counter + 1
+            printfn "Counter = %d" counter
+    
+    interface IDisposable with
+        member this.Dispose() =
+            printfn "MyService.Dispose()"
+
+
+let uri = new Uri("net.tcp://localhost")
+let binding = new NetTcpBinding()
+let host = new ServiceHost(typeof<MyService>, [| uri |])
+host.AddServiceEndpoint(typeof<IMyContract>, binding, "")
+host.Open()
+
+let proxy = ChannelFactory<IMyContract>.CreateChannel(binding, new EndpointAddress(string uri))
+
+proxy.MyMethod()
+proxy.MyMethod()
+
+(proxy :?> ICommunicationObject).Close()
+host.Close()
