d8fdccdb6b059fea7cfb5a21122af5f8ff8812ec
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-23 13:37:33 +0000

    Relocate more examples.

--- FSharpExamples/Faults/Declarative Exceptions Inclusion.fsx
+++ FSharpExamples/Faults/Declarative Exceptions Inclusion.fsx
@@ -1,11 +1,10 @@
 #light
 #r "System.ServiceModel"
 #r "System.Runtime.Serialization"
-#load "InProcHost.fsx"
-open Mcts_70_503
 open System
 open System.Runtime.Serialization
 open System.ServiceModel
+open System.ServiceModel.Channels
 
 
 [<ServiceContract>]
@@ -21,16 +20,18 @@ type MyService() =
             raise (new InvalidOperationException("Some error"))
 
 
-let host = new InProcHost<MyService>()
-host.AddEndpoint<IMyContract>()
+let uri = new Uri("net.tcp://localhost")
+let binding = new NetTcpBinding()
+let host = new ServiceHost(typeof<MyService>, [| uri |])
+host.AddServiceEndpoint(typeof<IMyContract>, binding, "")
 host.Open()
 
-let proxy = host.CreateProxy<IMyContract>()
+let proxy = ChannelFactory<IMyContract>.CreateChannel(binding, new EndpointAddress(string uri))
 try
     proxy.MethodWithError()
 with ex -> printfn "%s: %s\n%s" (ex.GetType().Name) ex.Message ex.StackTrace
 
 try
-    host.CloseProxy(proxy)
+    (proxy :?> ICommunicationObject).Close()
 with _ -> ()
 host.Close()
