d9b8425bb8a9401db51865935b6d24c2fe27d20a
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-23 14:27:24 +0000

    Yet more example relocates.

--- FSharpExamples/Faults/One-Way Operations And Exceptions.fsx
+++ FSharpExamples/Faults/One-Way Operations And Exceptions.fsx
@@ -1,11 +1,11 @@
 #light
 #r "System.ServiceModel"
 #r "System.Runtime.Serialization"
-#load "InProcHost.fsx"
-open Mcts_70_503
 open System
 open System.ServiceModel
+open System.ServiceModel.Channels
 open System.ServiceModel.Description
+
 
 [<ServiceContract>]
 type IMyContract =
@@ -15,6 +15,7 @@ type IMyContract =
     [<OperationContract>]
     abstract MethodWithoutError : unit -> unit
 
+[<ServiceBehavior(IncludeExceptionDetailInFaults = true)>]
 type MyService() =
     
     interface IMyContract with
@@ -22,17 +23,25 @@ type MyService() =
         
         member this.MethodWithoutError() = printfn "MyService.MethodWithoutError()"
 
-let host = new InProcHost<MyService>()
-host.AddEndpoint<IMyContract>()
+
+let uri = new Uri("net.tcp://localhost")
+let binding = new NetTcpBinding()
+let host = new ServiceHost(typeof<MyService>, [| uri |])
+host.AddServiceEndpoint(typeof<IMyContract>, binding, "")
 host.Open()
 
-let proxy = host.CreateProxy<IMyContract>()
+let proxy = ChannelFactory<IMyContract>.CreateChannel(binding, new EndpointAddress(string uri))
+
+// Run with and without following line
 proxy.MethodWithError()
 try
     // will error because channel is at fault
     proxy.MethodWithoutError()    
 with ex -> printfn "\n%s\n" ex.Message
 
-printfn "%A" host.InnerHost.State
+printfn "%A" host.State
 
+try
+    (proxy :?> ICommunicationObject).Close()
+with _ -> ()
 host.Close()
--- FSharpExamples/Faults/Suppressing Fault Contracts.fsx
+++ FSharpExamples/Faults/Suppressing Fault Contracts.fsx
@@ -1,27 +1,35 @@
 #light
 #r "System.ServiceModel"
 #r "System.Runtime.Serialization"
-#load "InProcHost.fsx"
-open Mcts_70_503
 open System
 open System.ServiceModel
 open System.ServiceModel.Channels
 open System.ServiceModel.Description
 open System.ServiceModel.Dispatcher
 
+
 [<ServiceContract>]
 type ICalculator =
     [<OperationContract>]
+    abstract Add : double * double -> double
+    
+    [<OperationContract>]
+    [<FaultContract(typeof<DivideByZeroException>)>]
     abstract Divide : double * double -> double
+
 
 type Calculator() =
     interface ICalculator with
+        member this.Add(n1, n2) =
+            n1 + n2
+            
         member this.Divide(n1, n2) =
-            match n2 with
-            | 0.0  ->
-                raise (new DivideByZeroException())
-            | _    -> n1 / n2
-
+            if n2 = 0.0 then
+                let ex = new DivideByZeroException()
+                raise (new FaultException<DivideByZeroException>(ex, "n2 is 0"))
+            n1 / n2
+            
+            
 type MyErrorHandler() =
     interface IErrorHandler with
         member this.HandleError(error) =
@@ -30,9 +38,7 @@ type MyErrorHandler() =
         
         member this.ProvideFault(error, version, fault) =
             printfn "MyErrorHandler.ProvideFault(): %A" fault
-            let faultException = new FaultException<int>(3)
-            let messageFault = faultException.CreateMessageFault()
-            fault <- Message.CreateMessage(version, messageFault, faultException.Action)
+            fault <- null
 
 
 type ErrorServiceBehavior() =
@@ -48,23 +54,26 @@ type ErrorServiceBehavior() =
             ()
 
 
-let host = new InProcHost<Calculator>()
-host.AddEndpoint<ICalculator>()
-host.InnerHost.Description.Behaviors.Add(new ErrorServiceBehavior())
+let uri = new Uri("net.tcp://localhost")
+let binding = new NetTcpBinding()
+let host = new ServiceHost(typeof<Calculator>, [| uri |])
+host.AddServiceEndpoint(typeof<ICalculator>, binding, "")
+// Enable and disable the following line to see the difference
+host.Description.Behaviors.Add(new ErrorServiceBehavior())
 host.Open()
 
-
-let proxy = host.CreateProxy<ICalculator>()
+let proxy = ChannelFactory<ICalculator>.CreateChannel(binding, new EndpointAddress(string uri))
+printfn "%f / %f = %f" 4.0 2.0 (proxy.Divide(4.0, 2.0))
 
 try
     proxy.Divide(4.0, 0.0) |> ignore
-with ex ->
-    printfn "%A: %s\n\n" (ex.GetType()) ex.Message
-    printfn "Proxy state = %A\n\n" (proxy :?> ICommunicationObject).State
+with ex -> 
+    printfn "%A: %s" (ex.GetType()) ex.Message
 
-try
-    host.CloseProxy(proxy)
-with _ -> ()
+// When the service throws an exception listed in the service-side fault
+//  contract, the exception will not fault the communication channel.
+printfn "Proxy state = %A" (proxy :?> ICommunicationObject).State
+
+(proxy :?> ICommunicationObject).Close()
 host.Close()
 
-Threading.Thread.Sleep(100)
