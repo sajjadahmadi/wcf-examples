3d5e5648e3446694418839a17d3c405aba549dcc
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-30 18:15:51 +0000

    Added Data Members.cs and Enumerations.cs  (Not complete examples, but a start) Changed CodeRunner namespace to WcfExamples

---
+++ InCSharp/Contracts/Data Contracts/Enumerations.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace WcfExamples
+{
+    [DataContract(Namespace =
+        "http://schemas.consumersenergy.com/2008/12/wcfexamples/")]
+    public enum MyEnumeration : int
+    {
+        [EnumMember]
+        One = 1,
+
+        [EnumMember]
+        Two = 2,
+
+        [EnumMember]
+        Three = 3
+    }
+}
