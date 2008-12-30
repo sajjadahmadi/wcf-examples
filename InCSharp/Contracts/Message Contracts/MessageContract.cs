a5509a01c4cebb9fb93f2aa3b4a733f6b6d0de57
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-30 18:14:04 +0000

    Moved and renamed MessageContractExample Changed MessageContractExample to a test project Moved MessageHeader example to the Message Contracts.csproj and changed it to a test project

---
+++ InCSharp/Contracts/Message Contracts/MessageContract.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfExamples.MessageContracts
+{
+    [TestClass]
+    public class MessageContractExample
+    {
+        #region Contracts
+        [DataContract]
+        class ContactInfo
+        {
+            [DataMember]
+            public string PhoneNumber;
+
+            [DataMember]
+            public string EmailAddress;
+        }
+
+        [MessageContract(IsWrapped = false)]
+        class ContactInfoRequestMessage
+        {
+            [MessageHeader]
+            public string LicenceKey;
+        }
+
+        [MessageContract(IsWrapped = false)]
+        class ContactInfoResponseMessage
+        {
+            [MessageBodyMember]
+            public ContactInfo ProviderContactInfo;
+        }
+        #endregion
+
+        #region Service
+        [ServiceContract]
+        interface ISomeService
+        {
+            [OperationContract]
+            [FaultContract(typeof(string))]
+            ContactInfoResponseMessage GetProviderContactInfo(
+                ContactInfoRequestMessage reqMsg);
+        }
+
+        class SomeService : ISomeService
+        {
+            public ContactInfoResponseMessage GetProviderContactInfo(ContactInfoRequestMessage reqMsg)
+            {
+                if (reqMsg.LicenceKey != "some valid key")
+                {
+                    throw new FaultException<string>("Invalid license key.");
+                }
+
+                ContactInfoResponseMessage respMsg =
+                    new ContactInfoResponseMessage();
+                respMsg.ProviderContactInfo = new ContactInfo();
+                respMsg.ProviderContactInfo.EmailAddress = "some@email.com";
+                respMsg.ProviderContactInfo.PhoneNumber = "555-555-5555";
+
+                return respMsg;
+            }
+        }
+        #endregion
+
+        #region Client
+        class SomeServiceClient : ClientBase<ISomeService>, ISomeService
+        {
+            public SomeServiceClient(string address)
+                : base(new NetNamedPipeBinding(), new EndpointAddress(address)) { }
+
+            public ContactInfoResponseMessage GetProviderContactInfo(ContactInfoRequestMessage reqMsg)
+            {
+                return Channel.GetProviderContactInfo(reqMsg);
+            }
+
+        }
+        #endregion
+
+        [TestMethod]
+        public void MessageContractTest()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(SomeService)))
+            {
+                string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+                host.AddServiceEndpoint(typeof(ISomeService), new NetNamedPipeBinding(), address);
+                host.Open();
+
+                using (SomeServiceClient proxy = new SomeServiceClient(address))
+                {
+                    proxy.Open();
+
+                    ContactInfoRequestMessage reqMsg = new ContactInfoRequestMessage();
+                    reqMsg.LicenceKey = "some valid key";
+
+                    ContactInfoResponseMessage respMsg;
+                    respMsg = proxy.GetProviderContactInfo(reqMsg);
+
+                    Assert.AreEqual("555-555-5555", respMsg.ProviderContactInfo.PhoneNumber);
+                    Assert.AreEqual("some@email.com", respMsg.ProviderContactInfo.EmailAddress);
+                }
+            }
+        }
+    }
+}
