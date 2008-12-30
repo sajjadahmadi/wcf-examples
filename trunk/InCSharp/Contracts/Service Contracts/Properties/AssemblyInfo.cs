e42b63fc56e177d4ff20f38936957a1141caf6dc
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2008-12-30 18:19:33 +0000

    Added Service Contracts and removed the empty Service folder Removed Message Header project (now contained within Message Contracts)

--- trunk/InCSharp/Contracts/Service Contracts/Properties/AssemblyInfo.cs
+++ trunk/InCSharp/Contracts/Service Contracts/Properties/AssemblyInfo.cs
@@ -4,22 +4,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Using MessageHeader")]
+[assembly: AssemblyTitle("Service Contracts")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Consumers Energy")]
-[assembly: AssemblyProduct("Using MessageHeader")]
+[assembly: AssemblyProduct("Service Contracts")]
 [assembly: AssemblyCopyright("Copyright © Consumers Energy 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b92e88fd-26b5-4efc-a1ca-cd53cf50e817")]
+[assembly: Guid("bc424ee8-6a57-47a4-912c-2fcd179ba484")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,8 +28,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
