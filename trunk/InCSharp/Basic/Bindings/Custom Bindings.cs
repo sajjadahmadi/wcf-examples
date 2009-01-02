2bb9b4aa21f5eee467bb0da40112852066d7e865
mark.alan.good@gmail.com <mark.alan.good@gmail.com@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-02 14:22:20 +0000

    (Becky committed these changes for me.) Added a Basic/Bindings project and a Custom Bindings example.  (The example doesn't work yet.  I'm trying to figure out why.)

---
+++ trunk/InCSharp/Basic/Bindings/Custom Bindings.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace WcfExamples.Bindings
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class CustomeBindingExample
+    {
+        [ServiceContract(Name = "MyService")]
+        interface IMyContract
+        {
+            [OperationContract]
+            void MyMethod();
+        }
+
+        class MyService : IMyContract
+        {
+            public void MyMethod()
+            {
+                // Do something
+            }
+        }
+
+        class MyServiceClient : ClientBase<IMyContract>, IMyContract
+        {
+            public MyServiceClient(Binding binding, string address)
+                : base(binding, new EndpointAddress(address))
+            { }
+
+            public void MyMethod()
+            {
+                Channel.MyMethod();
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string address = "http://localhost:8000/MyService" //+ Guid.NewGuid().ToString();
+            using (ServiceHost host = new ServiceHost(typeof(MyService)))
+            {
+                SymmetricSecurityBindingElement ssbe = new SymmetricSecurityBindingElement();
+                ssbe.LocalServiceSettings.InactivityTimeout = new TimeSpan(0, 10, 0);
+
+                BindingElementCollection bec = new BindingElementCollection();
+                // The recommended order for BindingElements is: 
+                // TransactionFlow, 
+                // ReliableSession, 
+                // Security, 
+                // CompositeDuplex, 
+                // OneWay, 
+                // StreamSecurity, 
+                // MessageEncoding, 
+                // Transport.
+                bec.Add(ssbe);
+                bec.Add(new TextMessageEncodingBindingElement());
+                bec.Add(new HttpTransportBindingElement());
+
+                CustomBinding binding = new CustomBinding(bec);
+
+                host.AddServiceEndpoint(
+                    typeof(IMyContract),
+                    binding,
+                    address);
+
+                MyServiceClient proxy = new MyServiceClient(binding, address);
+                proxy.Open();
+                proxy.MyMethod();
+
+                proxy.Close();
+            }
+        }
+    }
+}
