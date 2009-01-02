b06f60f31821153de2f857db1ba52fce6d12e3fb
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-02 19:46:02 +0000

    Added Data Contracts/Collections.cs

---
+++ InCSharp/Contracts/Data Contracts/Collections.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Diagnostics;
+
+namespace WcfExamples.CollectionDataContract
+{
+    [CollectionDataContract(Name = "MyCollectionOf{0}")]
+    class MyCollection<T>
+        : List<T>
+    {
+    }
+
+    [TestClass]
+    public class CollectionDataContractExample
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var c = new MyCollection<string>();
+            c.Add("item 1");
+            c.Add("item 2");
+            c.Add("item 3");
+
+            var serializer = new DataContractSerializer(c.GetType());
+            var stream = new MemoryStream();
+            serializer.WriteObject(stream, c);
+
+            stream.Position = 0;
+
+            var reader = new StreamReader(stream);
+            var doc = XDocument.Parse(reader.ReadToEnd());
+            const string exp =
+@"<MyCollectionOfstring xmlns=""http://schemas.datacontract.org/2004/07/WcfExamples.CollectionDataContract"" xmlns:i=""http://www.w3.org/2001/XMLSchema-instance"">
+  <string>item 1</string>
+  <string>item 2</string>
+  <string>item 3</string>
+</MyCollectionOfstring>";
+
+            Assert.AreEqual(exp, doc.ToString());
+        }
+    }
+}
