599ce654e2ace5779f70d2a6e2d5436a433588eb
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-06 17:25:05 +0000

    Changed project to a Test Project Removed Sub-main and added Using Proxies test class.

---
+++ InCSharp/Basic/Proxy/Using Proxies.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+
+namespace WcfExamples
+{
+    [TestClass]
+    public class Program
+    {
+        // Service
+        class MyService : IMyContract
+        {
+            public string MyOperation() {
+                return "Return Value";
+            }
+        }
+
+        #region Host
+        static string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+        static ServiceHost host;
+
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext) {
+            host = new ServiceHost(typeof(MyService));
+            host.AddServiceEndpoint(typeof(IMyContract), new NetNamedPipeBinding(), address);
+            host.Open();
+        }
+
+        [ClassCleanup()]
+        public static void MyClassCleanup() {
+            if (host.State == CommunicationState.Opened)
+                host.Close();
+        }
+        #endregion
+
+        [TestMethod]
+        public void TestToolGeneratedProxy() {
+            ToolGeneratedProxy proxy = new ToolGeneratedProxy(new NetNamedPipeBinding(), new EndpointAddress(address));
+            Assert.AreEqual("Return Value", proxy.MyOperation());
+            proxy.Close();
+        }
+    }
+}
