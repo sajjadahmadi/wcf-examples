8866bfaecf6053ab31d1465b577a4e10fe9603ce
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-06 16:46:42 +0000

    Added IsRequired and EmitDefaultValue examples

--- trunk/InCSharp/Contracts/Data Contracts/Data Members.cs
+++ trunk/InCSharp/Contracts/Data Contracts/Data Members.cs
@@ -1,16 +1,43 @@
 ﻿using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System;
 
 namespace WcfExamples
 {
-    [DataContract(Namespace =
-        "http://schemas.consumersenergy.com/2008/12/wcfexamples/")]
-    public class MemberInfo
+    [TestClass]
+    public class DataMemberExample
     {
-        [DataMember(
-            Name = "MemberAlias",
-            EmitDefaultValue = false,
-            IsRequired = true,
-            Order = 0)]
-        public string MemberString;
+        [DataContract]
+        public class DataMemberIsRequired
+        {
+            [DataMember(IsRequired = true)]
+            public string MemberString;
+        }
+
+        [DataContract]
+        public class DataMemberEmitDefaultValueFalse
+        {
+            [DataMember(EmitDefaultValue = false)]
+            public string MemberString;
+        }
+
+
+        [TestMethod]
+        public void EmitDefaultValueFalse() {
+            var data = new DataMemberEmitDefaultValueFalse();
+            var s = DataContractSerializer<DataMemberEmitDefaultValueFalse>.Serialize(data);
+            var doc = XDocument.Parse(s);
+            string expected = "<DataMemberExample.DataMemberEmitDefaultValue xmlns=\"http://schemas.datacontract.org/2004/07/WcfExamples\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" />";
+            Assert.AreEqual(expected, doc.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SerializationException))]
+        public void MissingMemberThatIsRequired() {
+            var missingMember = "<DataMemberExample.DataMemberIsRequired xmlns=\"http://schemas.datacontract.org/2004/07/WcfExamples\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" />";
+            var obj = DataContractSerializer<DataMemberIsRequired>.Deserialize(missingMember);
+        }
     }
 }
