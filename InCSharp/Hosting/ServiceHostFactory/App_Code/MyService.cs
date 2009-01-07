added97c3f7e3254b2c04de20c10a0465f9499fa
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-07 18:31:44 +0000

    Added ServiceHostFactory example

---
+++ InCSharp/Hosting/ServiceHostFactory/App_Code/MyService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Diagnostics;
+
+[ServiceContract]
+public interface IMyContract
+{
+    [OperationContract]
+    string MyMethod();
+}
+
+public class MyService : IMyContract
+{
+    public string MyMethod() {
+        return "Do something";
+    }
+}
