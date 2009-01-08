4022fdb05f20c5d1b57d00a09403cf488ebee17a
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-08 13:52:09 +0000

    Simplified Throttling examples

--- trunk/InCSharp/Instance Management/Throttling.cs
+++ trunk/InCSharp/Instance Management/Throttling.cs
@@ -2,6 +2,8 @@
 using System.ServiceModel.Dispatcher;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Runtime.Serialization;
+using System.ServiceModel.Channels;
+using System.Threading;
 
 namespace System.ServiceModel.Examples
 {
@@ -27,25 +29,16 @@ namespace System.ServiceModel.Examples
         }
 
         [ServiceContract]
-        interface IThrottlingInformation
+        interface IMyContract
         {
             [OperationContract]
             ThrottleInfo GetThrottleInfo();
         }
 
-        [ServiceContract]
-        interface IMyContract
-        {
-            [OperationContract]
-            bool MyMethod();
-        }
-
         // Service
         [ServiceBehavior]
-        class ThrottledService : IThrottlingInformation, IMyContract
+        class ThrottledService : IMyContract
         {
-            public bool MyMethod() { return true; }
-
             public ThrottleInfo GetThrottleInfo() { return new ThrottleInfo(MyThrottle); }
 
             private ServiceThrottle MyThrottle
@@ -59,7 +52,19 @@ namespace System.ServiceModel.Examples
                     return serviceThrottle;
                 }
             }
+        }
 
+        // Client
+        class MyContractClient : ClientBase<IMyContract>, IMyContract
+        {
+            public MyContractClient(Binding binding, string address)
+                : base(binding, new EndpointAddress(address))
+            { }
+
+            public ThrottleInfo GetThrottleInfo()
+            {
+                return Channel.GetThrottleInfo();
+            }
         }
 
         [TestMethod]
@@ -72,17 +77,17 @@ namespace System.ServiceModel.Examples
             {
                 // Throttle set here
                 ServiceThrottlingBehavior throttle = new ServiceThrottlingBehavior();
+                throttle.MaxConcurrentCalls = 12;
+                throttle.MaxConcurrentSessions = 34;
+                throttle.MaxConcurrentInstances = 56;
                 host.Description.Behaviors.Add(throttle);
-                host.AddServiceEndpoint(typeof(IThrottlingInformation), binding, "");
+                host.AddServiceEndpoint(typeof(IMyContract), binding, "");
                 host.Open();
 
                 // Throttle read here
-                IThrottlingInformation service = ChannelFactory<IThrottlingInformation>.CreateChannel(
-                    binding,
-                    new EndpointAddress(address));
-                using (service as IDisposable)
+                using (MyContractClient proxy = new MyContractClient(binding, address))
                 {
-                    ThrottleInfo info = service.GetThrottleInfo();
+                    ThrottleInfo info = proxy.GetThrottleInfo();
                     Assert.AreEqual(12, info.MaxConcurrentCalls);
                     Assert.AreEqual(34, info.MaxConcurrentSessions);
                     Assert.AreEqual(56, info.MaxConcurrentInstances);
@@ -96,7 +101,6 @@ namespace System.ServiceModel.Examples
             "The open operation did not complete within the allotted timeout of 00:00:01. The time allotted to this operation may have been a portion of a longer timeout.")]
         public void MaxSessions()
         {
-            NetNamedPipeBinding binding = new NetNamedPipeBinding();
             string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
 
             using (ServiceHost host = new ServiceHost(typeof(ThrottledService), new Uri(address)))
@@ -105,61 +109,21 @@ namespace System.ServiceModel.Examples
                 ServiceThrottlingBehavior throttle = new ServiceThrottlingBehavior();
                 throttle.MaxConcurrentSessions = 1;
                 host.Description.Behaviors.Add(throttle);
-                host.AddServiceEndpoint(typeof(IMyContract), binding, "");
+                host.AddServiceEndpoint(typeof(IMyContract), new NetNamedPipeBinding(), "");
                 host.Open();
                 Assert.AreEqual(CommunicationState.Opened, host.State);
 
-                IMyContract proxy1 = ChannelFactory<IMyContract>.CreateChannel(
-                    new NetNamedPipeBinding(),
-                    new EndpointAddress(address));
-
-                using (proxy1 as IDisposable)
+                // Try to start two sessions
+                using (MyContractClient proxy1 = new MyContractClient(new NetNamedPipeBinding(), address))
                 {
-                    Assert.AreEqual(true, proxy1.MyMethod());
+                    proxy1.Open();
 
-                    IMyContract proxy2;
-                    proxy2 = ChannelFactory<IMyContract>.CreateChannel(
-                        new NetNamedPipeBinding() { SendTimeout = new TimeSpan(0, 0, 1) },
-                        new EndpointAddress(address));
-                    Assert.AreEqual(true, proxy2.MyMethod());
-                    Assert.Fail("Expected SendTimeout exception.");
+                    NetNamedPipeBinding binding = new NetNamedPipeBinding() { OpenTimeout = new TimeSpan(0, 0, 1) };
+                    MyContractClient proxy2 = new MyContractClient(binding, address);
+                    proxy2.Open(); // Should timeout
                 }
             }
-
         }
 
-        // TODO: Fix broken test!
-        [TestMethod]
-        public void BindingMaxConnections()
-        {
-            string address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
-            ServiceHost<ThrottledService> host = new ServiceHost<ThrottledService>(address);
-            NetNamedPipeBinding binding = new NetNamedPipeBinding();
-            host.OpenTimeout = new TimeSpan(0, 0, 30);
-            binding.MaxConnections = 1;
-            host.AddServiceEndpoint(typeof(IMyContract), binding, "");
-            host.Open();
-
-            IMyContract service1 = ChannelFactory<IMyContract>.CreateChannel(
-                new NetNamedPipeBinding(),
-                new EndpointAddress(address));
-            Assert.AreEqual(true, service1.MyMethod());
-
-            try
-            {
-                IMyContract service2 = ChannelFactory<IMyContract>.CreateChannel(
-                    new NetNamedPipeBinding() { SendTimeout = new TimeSpan(0, 0, 2) },
-                    new EndpointAddress(address));
-                Assert.AreEqual(true, service2.MyMethod());
-                ((ICommunicationObject)service2).Close();
-                Assert.Fail("Expected SendTimeout exception.");
-            }
-            catch (TimeoutException) { }
-            finally
-            {
-                ((ICommunicationObject)service1).Close();
-                host.Close();
-            }
-        }
     }
 }
