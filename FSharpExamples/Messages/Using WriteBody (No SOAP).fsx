ed80d3b18d0c8733cdf68a49b5172048af10b9e9
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-15 20:04:31 +0000

    Add create Message examples.

---
+++ FSharpExamples/Messages/Using WriteBody (No SOAP).fsx
@@ -0,0 +1,22 @@
+#light
+#r "System.Runtime.Serialization"
+#r "System.ServiceModel"
+open System.IO
+open System.Xml
+open System.ServiceModel.Channels
+
+let body = "Body"
+
+let stream = new MemoryStream()
+let xmlWriter = XmlDictionaryWriter.CreateTextWriter(stream)
+
+let v = MessageVersion.None
+let msg = Message.CreateMessage(v, "action", body)
+msg.WriteBody(xmlWriter)
+
+xmlWriter.Flush()
+stream.Position <- 0L
+let reader = new StreamReader(stream)
+printfn "Message"
+printfn "  Status: %A" msg.State
+printfn "  Body: %s" (reader.ReadToEnd())
