32fd33fbec5a01f6caccbefe438e2dee8c878559
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-01-20 21:26:24 +0000

    Added End-to-End Tracing example

---
+++ trunk/InCSharp/Instrumentation/End-to-End Tracing/Service/IMyContract.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfExamples.EndToEndTracing.Service
+{
+    [ServiceContract(Name = "MyService", Namespace = "http://tempuri.org")]
+    public interface IMyContract
+    {
+        [OperationContract]
+        [FaultContract(typeof(string))]
+        string GetHeader(string name, string ns);
+    }
+}
---
+++ trunk/InCSharp/Instrumentation/End-to-End Tracing/Service/MyService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Channels;
+using System.Diagnostics;
+
+namespace WcfExamples.EndToEndTracing.Service
+{
+    public class MyService : IMyContract
+    {
+        public string GetHeader(string name, string ns)
+        {
+            Debug.Assert(name != null);
+            Debug.Assert(ns != null);
+
+            TraceSource ts = new TraceSource("MyServiceTraceSource");
+            if (Trace.CorrelationManager.ActivityId == Guid.Empty)
+                Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+            ts.TraceEvent(TraceEventType.Start, 0, "GetHeader Activity");
+
+            try
+            {
+                Message reqMessage = OperationContext.Current.RequestContext.RequestMessage;
+                int headerIndex = reqMessage.Headers.FindHeader(name, ns);
+                if (headerIndex != -1)
+                    return reqMessage.Headers.GetHeader<string>(headerIndex);
+                else
+                    throw new FaultException<string>(reqMessage.Headers.Action, "Header not found.  See detail for Action header.");
+            }
+            finally
+            {
+                ts.TraceEvent(TraceEventType.Stop, 0, "GetHeader Activity");
+            }
+        }
+
+    }
+}
