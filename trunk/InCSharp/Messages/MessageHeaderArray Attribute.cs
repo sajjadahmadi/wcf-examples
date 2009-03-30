882e1f0484569f28d3a10048e569fcb0164a278d
mark.alan.good@gmail.com <mark.alan.good@gmail.com@7ff4561e-8701-11dd-9205-735403a6da14>
2009-03-30 00:15:06 +0000

    Added MessageHeaderArray Attribute example

---
+++ trunk/InCSharp/Messages/MessageHeaderArray Attribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Messages
+{
+    [MessageContract]
+    class MailMessage
+    {
+        /// <summary>
+        /// Specifies that the default wrapper element in the 
+        /// SOAP message must NOT be written around array types in a header element.
+        /// For example:
+        /// <To>user1@company.com</To>
+        /// <To>user2@company.com</To>
+        /// <To>user3@company.com</To>
+        /// </summary>
+        [MessageHeaderArray]
+        public string[] To;
+
+        /// <summary>
+        /// Specifies that a data member is a SOAP message header.
+        /// </summary>
+        [MessageHeader]
+        public string From;
+
+        /// <summary>
+        /// Specifies that a member is serialized as an element inside the SOAP body.
+        /// </summary>
+        [MessageBodyMember]
+        public string Subject;
+
+        [MessageBodyMember]
+        public string Body;
+    }
+}
