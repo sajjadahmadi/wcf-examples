7cc4febae1187b773795a9bfe46f5534769be088
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-03-31 19:06:54 +0000

    Changed indentation spaces to tabs

--- trunk/InCSharp/Callbacks/Events/Events.cs
+++ trunk/InCSharp/Callbacks/Events/Events.cs
@@ -2,174 +2,175 @@
 
 namespace System.ServiceModel.Examples
 {
-    enum EventType
-    {
-        Event1 = 1,
-        Event2 = 2,
-        Event3 = 4,
-        AllEvents = Event1 | Event2 | Event3
-    }
+	enum EventType
+	{
+		Event1 = 1,
+		Event2 = 2,
+		Event3 = 4,
+		AllEvents = Event1 | Event2 | Event3
+	}
 
-    interface IMyEvents
-    {
-        [OperationContract(IsOneWay = true)]
-        void OnEvent1();
-        [OperationContract(IsOneWay = true)]
-        void OnEvent2(int number);
-        [OperationContract(IsOneWay = true)]
-        void OnEvent3(int number, string text);
-    }
 
-    [ServiceContract]
-    interface ISubscriptionService
-    {
-        [OperationContract]
-        void Subscribe(EventType mask);
-        [OperationContract]
-        void Unsubscribe(EventType mask);
-    }
+	[ServiceContract]
+	interface ISubscriptionService
+	{
+		[OperationContract]
+		void Subscribe(EventType mask);
+		[OperationContract]
+		void Unsubscribe(EventType mask);
+	}
 
-    [ServiceContract(CallbackContract = typeof(IMyEvents))]
-    interface IContractWithEvents : ISubscriptionService
-    {
-        [OperationContract]
-        void FireEvents(EventType eventType);
-    }
+	[ServiceContract(CallbackContract = typeof(IPublisherEvents))]
+	interface IPublisherService
+	{
+		[OperationContract]
+		void FireEvents(EventType eventType);
+	}
 
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
-    class MyPublisher : IContractWithEvents
-    {
-        public delegate void GenericEventHandler();
-        public delegate void GenericEventHandler<T>(T t);
-        public delegate void GenericEventHandler<T, U>(T t, U u);
+	interface IPublisherEvents
+	{
+		[OperationContract(IsOneWay = true)]
+		void OnEvent1();
+		[OperationContract(IsOneWay = true)]
+		void OnEvent2(int number);
+		[OperationContract(IsOneWay = true)]
+		void OnEvent3(int number, string text);
+	}
 
-        static GenericEventHandler event1 = delegate { };
-        static GenericEventHandler<int> event2 = delegate { };
-        static GenericEventHandler<int, string> event3 = delegate { };
+	[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+	class PublisherService : IPublisherService, ISubscriptionService
+	{
+		public delegate void GenericEventHandler();
+		public delegate void GenericEventHandler<T>(T t);
+		public delegate void GenericEventHandler<T, U>(T t, U u);
 
-        #region IContractWithEvents Members
+		static GenericEventHandler event1 = delegate { };
+		static GenericEventHandler<int> event2 = delegate { };
+		static GenericEventHandler<int, string> event3 = delegate { };
 
-        public void Subscribe(EventType mask)
-        {
-            IMyEvents subscriber = OperationContext.Current.GetCallbackChannel<IMyEvents>();
+		public void Subscribe(EventType mask)
+		{
+			IPublisherEvents subscriber = OperationContext.Current.GetCallbackChannel<IPublisherEvents>();
 
-            if ((mask & EventType.Event1) == EventType.Event1)
-            { event1 += subscriber.OnEvent1; }
-            if ((mask & EventType.Event2) == EventType.Event2)
-            { event2 += subscriber.OnEvent2; }
-            if ((mask & EventType.Event3) == EventType.Event3)
-            { event3 += subscriber.OnEvent3; }
-        }
+			if ((mask & EventType.Event1) == EventType.Event1)
+			{ event1 += subscriber.OnEvent1; }
+			if ((mask & EventType.Event2) == EventType.Event2)
+			{ event2 += subscriber.OnEvent2; }
+			if ((mask & EventType.Event3) == EventType.Event3)
+			{ event3 += subscriber.OnEvent3; }
+		}
 
-        public void Unsubscribe(EventType mask)
-        {
-            IMyEvents subscriber = OperationContext.Current.GetCallbackChannel<IMyEvents>();
+		#region IContractWithEvents Members
 
-            if ((mask & EventType.Event1) == EventType.Event1)
-            { event1 -= subscriber.OnEvent1; }
-            if ((mask & EventType.Event2) == EventType.Event2)
-            { event2 -= subscriber.OnEvent2; }
-            if ((mask & EventType.Event3) == EventType.Event3)
-            { event3 -= subscriber.OnEvent3; }
-        }
+		public void Unsubscribe(EventType mask)
+		{
+			IPublisherEvents subscriber = OperationContext.Current.GetCallbackChannel<IPublisherEvents>();
 
-        public void FireEvents(EventType eventType)
-        {
-            switch (eventType)
-            {
-                case EventType.Event1:
-                    { event1(); break; }
-                case EventType.Event2:
-                    { event2(30); break; }
-                case EventType.Event3:
-                    { event3(60, "Some Text"); break; }
-                case EventType.AllEvents:
-                    {
-                        event1();
-                        event2(50);
-                        event3(100, "All");
-                        break;
-                    }
-                default:
-                    { throw new InvalidOperationException("Unknown event!"); }
-            }
-        }
+			if ((mask & EventType.Event1) == EventType.Event1)
+			{ event1 -= subscriber.OnEvent1; }
+			if ((mask & EventType.Event2) == EventType.Event2)
+			{ event2 -= subscriber.OnEvent2; }
+			if ((mask & EventType.Event3) == EventType.Event3)
+			{ event3 -= subscriber.OnEvent3; }
+		}
 
-        #endregion
-    }
+		public void FireEvents(EventType eventType)
+		{
+			switch (eventType)
+			{
+				case EventType.Event1:
+					{ event1(); break; }
+				case EventType.Event2:
+					{ event2(30); break; }
+				case EventType.Event3:
+					{ event3(60, "Some Text"); break; }
+				case EventType.AllEvents:
+					{
+						event1();
+						event2(50);
+						event3(100, "All");
+						break;
+					}
+				default:
+					{ throw new InvalidOperationException("Unknown event!"); }
+			}
+		}
 
-    class MySubscriber : IMyEvents
-    {
-        public EventType EventFired;
-        public int Number;
-        public string Text;
+		#endregion
+	}
 
-        #region IMyEvents Members
+	class MySubscriber : IPublisherEvents
+	{
+		public EventType EventFired;
+		public int Number;
+		public string Text;
 
-        public void OnEvent1()
-        { EventFired |= EventType.Event1; }
+		#region IMyEvents Members
 
-        public void OnEvent2(int number)
-        {
-            Number = number;
-            EventFired |= EventType.Event2;
-        }
+		public void OnEvent1()
+		{ EventFired |= EventType.Event1; }
 
-        public void OnEvent3(int number, string text)
-        {
-            Number = number;
-            Text = text;
-            EventFired |= EventType.Event3;
-        }
+		public void OnEvent2(int number)
+		{
+			Number = number;
+			EventFired |= EventType.Event2;
+		}
 
-        #endregion
-    }
+		public void OnEvent3(int number, string text)
+		{
+			Number = number;
+			Text = text;
+			EventFired |= EventType.Event3;
+		}
 
-    [TestClass]
-    public class Events
-    {
-        [TestMethod]
-        public void SubscribeToEvents()
-        {
-            string address = "net.pipe://localhost/";
-            using (ServiceHost<MyPublisher> host = new ServiceHost<MyPublisher>())
-            {
-                host.AddServiceEndpoint<IContractWithEvents>(new NetNamedPipeBinding(), address);
-                host.Open();
+		#endregion
+	}
 
-                MySubscriber client = new MySubscriber();
-                IContractWithEvents channel = DuplexChannelFactory<IContractWithEvents, IMyEvents>
-                    .CreateChannel(client, new NetNamedPipeBinding(), new EndpointAddress(address));
+	[TestClass]
+	public class Events
+	{
+		[TestMethod]
+		public void SubscribeToEvents()
+		{
+			string address = "net.pipe://localhost/";
+			using (ServiceHost<PublisherService> host = new ServiceHost<PublisherService>())
+			{
+				host.AddServiceEndpoint<IPublisherService>(new NetNamedPipeBinding(), address);
+				host.Open();
 
-                // Subscribed to All, but only fired Event1
-                channel.Subscribe(EventType.AllEvents);
-                channel.FireEvents(EventType.Event1);
-                Assert.AreEqual(EventType.Event1, (client.EventFired & EventType.Event1));
-                Assert.AreEqual(default(int), client.Number);
-                Assert.AreEqual(default(string), client.Text);
+				MySubscriber client = new MySubscriber();
+				IPublisherService channel = DuplexChannelFactory<IPublisherService, IPublisherEvents>
+					 .CreateChannel(client, new NetNamedPipeBinding(), new EndpointAddress(address));
 
-                channel.Unsubscribe(EventType.AllEvents);
+				// Subscribed to All, but only fired Event1
+				channel.Subscribe(EventType.AllEvents);
+				channel.FireEvents(EventType.Event1);
+				Assert.AreEqual(EventType.Event1, (client.EventFired & EventType.Event1));
+				Assert.AreEqual(default(int), client.Number);
+				Assert.AreEqual(default(string), client.Text);
 
-                // Fired All, but only subscribed to Event2
-                channel.Subscribe(EventType.Event2);
-                channel.FireEvents(EventType.AllEvents);
-                Assert.AreEqual(EventType.Event2, (client.EventFired & EventType.Event2));
-                Assert.AreNotEqual(EventType.AllEvents, (client.EventFired & EventType.AllEvents));
-                Assert.AreEqual(50, client.Number);
-                Assert.AreEqual(default(string), client.Text);
+				channel.Unsubscribe(EventType.AllEvents);
 
-                channel.Unsubscribe(EventType.AllEvents);
+				// Fired All, but only subscribed to Event2
+				channel.Subscribe(EventType.Event2);
+				channel.FireEvents(EventType.AllEvents);
+				Assert.AreEqual(EventType.Event2, (client.EventFired & EventType.Event2));
+				Assert.AreNotEqual(EventType.AllEvents, (client.EventFired & EventType.AllEvents));
+				Assert.AreEqual(50, client.Number);
+				Assert.AreEqual(default(string), client.Text);
 
-                // Subscribed to All, Fired All
-                channel.Subscribe(EventType.AllEvents);
-                channel.FireEvents(EventType.AllEvents);
-                Assert.AreEqual(EventType.AllEvents, (client.EventFired & EventType.AllEvents));
-                Assert.AreEqual(100, client.Number);
-                Assert.AreEqual("All", client.Text);
+				channel.Unsubscribe(EventType.AllEvents);
 
-                ((ICommunicationObject)channel).Close();
-            }
+				// Subscribed to All, Fired All
+				channel.Subscribe(EventType.AllEvents);
+				channel.FireEvents(EventType.AllEvents);
+				Assert.AreEqual(EventType.AllEvents, (client.EventFired & EventType.AllEvents));
+				Assert.AreEqual(100, client.Number);
+				Assert.AreEqual("All", client.Text);
 
-        }
-    }
+				((ICommunicationObject)channel).Close();
+			}
+
+		}
+	}
 }
