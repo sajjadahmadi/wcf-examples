c43f121d865aa03ed074dd2b75b526ae94433424
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-08-29 00:55:30 +0000

    Renaming

---
+++ trunk/InCSharp/Contracts/Data Contracts/DataMember.IsRequired.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfExamples
+{
+    [TestClass]
+    public class IsRequired
+    {
+        [TestMethod]
+        [ExpectedException(typeof (SerializationException))]
+        public void MissingMemberThatIsRequired()
+        {
+            const string missingMember =
+                "<DataMemberExample.DataMemberIsRequired xmlns=\"http://schemas.datacontract.org/2004/07/WcfExamples\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" />";
+            var obj = DataContractSerializer<DataMemberIsRequired>.Deserialize(missingMember);
+        }
+
+        #region Nested type: DataMemberIsRequired
+
+        [DataContract]
+        public class DataMemberIsRequired
+        {
+            [DataMember(IsRequired = true)] public string MemberString;
+        }
+
+        #endregion
+    }
+}
