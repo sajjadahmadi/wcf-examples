f33fa3432da23443dc52830724379aa802efa642
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2009-08-29 19:41:11 +0000

    Add Basic Trace example.

---
+++ trunk/FSharpExamples/Instrumenting and Administering/Implement Tracing/Basic Trace/Program.fs
@@ -0,0 +1,22 @@
+﻿open System.ServiceModel
+
+[<ServiceContract>]
+type IContract =
+    [<OperationContract>]
+    abstract Operation : unit -> unit
+    
+type Service() =
+    interface IContract with
+        member this.Operation() =
+            printfn "Service Operation Called"
+
+
+let host = new ExampleHost<Service, IContract>()
+host.Open()
+
+let proxy = host.CreateProxy()
+proxy.Operation()
+
+host.Close()
+printfn "Done!"
+
