3612a792c8a7ed910584da44f3fbea7a3cadaec3
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-08-29 00:18:13 +0000

    Added MissingRequiredMember.cs Renamed Data Members.cs to EmitDefaultValue.cs

---
+++ InCSharp/Contracts/Data Contracts/MissingRequiredMember.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Xml.Linq;
+using System;
+
+namespace WcfExamples
+{
+    [TestClass]
+    public class MissingRequiredMember
+    {
+        [DataContract]
+        public class DataMemberIsRequired
+        {
+            [DataMember(IsRequired = true)]
+            public string MemberString;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(SerializationException))]
+        public void MissingMemberThatIsRequired()
+        {
+            const string missingMember = "<DataMemberExample.DataMemberIsRequired xmlns=\"http://schemas.datacontract.org/2004/07/WcfExamples\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" />";
+            var obj = DataContractSerializer<DataMemberIsRequired>.Deserialize(missingMember);
+        }
+    }
+}
