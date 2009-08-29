98ba902640245f6057167ff42b2b6f44b43c0735
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-08-29 10:41:23 +0000

    Added DataContractFormat example

---
+++ InCSharp/Contracts/Data Contracts/DataContractFormat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfExamples
+{
+    [TestClass]
+    public class DataContractFormat
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // TODO
+        }
+
+        #region Nested type: ICalculator
+
+        [ServiceContract, DataContractFormat(Style = OperationFormatStyle.Rpc)]
+        private interface ICalculator
+        {
+            [OperationContract, DataContractFormat(Style = OperationFormatStyle.Rpc)]
+            double Add(double a, double b);
+
+            [OperationContract, DataContractFormat(Style = OperationFormatStyle.Document)]
+            double Subtract(double a, double b);
+        }
+
+        #endregion
+    }
+}
