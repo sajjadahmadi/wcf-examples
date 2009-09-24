b52067d97fcb996cf9d8bab697c8d2b86302e7a3
mark.alan.good <mark.alan.good@7ff4561e-8701-11dd-9205-735403a6da14>
2009-09-24 14:42:55 +0000

    Added IAuthorizationPolicy example

---
+++ InCSharp/Security/FederatedSecurity/ClaimsBasedServices/Over13AuthorizationPolicy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Policy;
+using System.ServiceModel;
+
+namespace ClaimsBasedServices
+{
+	class Over13AuthorizationPolicy : IAuthorizationPolicy
+	{
+		private readonly string _id;
+		private readonly ClaimSet _issuer;
+
+		public Over13AuthorizationPolicy()
+		{
+			_id = Guid.NewGuid().ToString();
+			_issuer = ClaimSet.System; // Current app is the issuer; app-trusted issuer
+		}
+
+		bool IAuthorizationPolicy.Evaluate(EvaluationContext evaluationContext, ref object state)
+		{
+			DateTime? birthDate = null;
+			var authorizationContext = ServiceSecurityContext.Current.AuthorizationContext;
+			foreach (var claimSet in authorizationContext.ClaimSets)
+			{
+				var claims = claimSet.FindClaims(ClaimTypes.DateOfBirth, Rights.PossessProperty);
+				foreach (var claim in claims)
+					birthDate = Convert.ToDateTime(claim.Resource);
+			}
+			return birthDate.HasValue 
+				? birthDate.Value <= DateTime.Now.AddYears(-13) 
+				: false;
+		}
+
+		ClaimSet IAuthorizationPolicy.Issuer
+		{
+			get { return _issuer; }
+		}
+
+		string IAuthorizationComponent.Id
+		{
+			get { return _id; }
+		}
+	}
+}
