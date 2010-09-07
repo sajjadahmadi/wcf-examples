9a76d040ed2517b0a45d121dd9b7d9e768c84ca6
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2010-09-07 15:10:13 +0000

    Updated and moved Http Services examples.

--- FSharpExamples/Reorg/Http Services/Basic Web-Style Service.fsx
+++ FSharpExamples/Reorg/Http Services/Basic Web-Style Service.fsx
@@ -1,10 +1,8 @@
 // Adapted from http://msdn.microsoft.com/en-us/library/bb412178.aspx
 #r "System.ServiceModel"
 #r "System.ServiceModel.Web"
+#load "_Service.fsx"
 
-#load "Service.fsx"
-
-Service.start()
 printfn "Press enter to quit..."
 System.Console.ReadLine() |> ignore
 
--- FSharpExamples/Reorg/Http Services/Call Web-Style Service with ChannelFactory.fsx
+++ FSharpExamples/Reorg/Http Services/Call Web-Style Service with ChannelFactory.fsx
@@ -1,21 +1,16 @@
 #r "System.ServiceModel"
 #r "System.ServiceModel.Web"
+#load "_Service.fsx"
 open System.ServiceModel
 open System.ServiceModel.Description
 
-#load "Service.fs"
-Service.start()
-
-let factory = new ChannelFactory<Service.IService>(new WebHttpBinding(), "http://localhost:8888")
+let factory = new ChannelFactory<Service.IService>(new WebHttpBinding(), "http://localhost:8000")
 factory.Endpoint.Behaviors.Add(new WebHttpBehavior())
-
 let channel = factory.CreateChannel()
 
 printfn "Calling EchoWithGet via HTTP GET: "
 let sGet = channel.EchoWithGet("Hello, world")
-printfn "    Output: %s" sGet
-
-printfn "\nThis can also be accomplished be navigating to <http://localhost:8000/EchoWithGet?s=Hello, world> in a web browser"
+printfn "    Output: %s\n" sGet
 
 printfn "Calling EchoWithPost via HTTP POST: "
 let sPost = channel.EchoWithPost("Hello, world")
