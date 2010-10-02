db949bd225c6825e4fda8b5fee45de3d5a0bd333
mark.alan.good <mark.alan.good@gmail.com>
2010-10-02 18:39:24 +0000

    Added BasicMessageQueue

---
+++ InCSharp/BasicMessageQueue/Contract/IMessagingService.cs
@@ -0,0 +1,11 @@
+﻿using System.ServiceModel;
+
+namespace QueuedCalls.Contract
+{
+    [ServiceContract(Namespace = "http://www.mynamespace.com/")]
+    public interface IMessagingService
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message);
+    }
+}
---
+++ InCSharp/BasicMessageQueue/Service/MessagingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ServiceModel;
+using QueuedCalls.Contract;
+
+namespace QueuedCalls.Service
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    internal class MessagingService : IMessagingService
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
