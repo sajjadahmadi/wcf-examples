25b968370e1ae2c8bad0d886cb7fc92e7e7b6b83
mark.alan.good <mark.alan.good@gmail.com>
2010-10-04 00:55:56 +0000

    Separated Services and Client

---
+++ InCSharp/Simple Routing/Simple Routing Service/Greeting.Client/IGreetingService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+
+namespace Greeting.Client
+{
+    [ServiceContract]
+    internal interface IGreetingService
+    {
+        [OperationContract(Action = "Hello")]
+        string SayHello();
+
+        [OperationContract(Action = "Goodbye")]
+        string SayGoodbye();        
+    }
+}
---
+++ InCSharp/Simple Routing/Simple Routing Service/Greeting.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace Greeting.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string routerAddress = "http://localhost:8001/router";
+            Binding routerBinding = new BasicHttpBinding();
+
+            var helloClient = ChannelFactory<IGreetingService>.CreateChannel(routerBinding,
+                                                                             new EndpointAddress(routerAddress));
+            var helloResult = helloClient.SayHello();
+            Console.WriteLine(helloResult);
+            ((ICommunicationObject) helloClient).Close();
+
+            var goodbyeClient = ChannelFactory<IGreetingService>.CreateChannel(routerBinding,
+                                                                               new EndpointAddress(routerAddress));
+            var goodbyeResult = goodbyeClient.SayGoodbye();
+            Console.WriteLine(goodbyeResult);
+            ((ICommunicationObject) goodbyeClient).Close();
+        }
+    }
+}
