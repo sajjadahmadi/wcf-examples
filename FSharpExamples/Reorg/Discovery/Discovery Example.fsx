c877a89d32842bf950f319ca5c9f8fcc123e1671
r.vernagus <r.vernagus@7ff4561e-8701-11dd-9205-735403a6da14>
2010-10-05 17:34:48 +0000

    Added Discovery Example.

---
+++ FSharpExamples/Reorg/Discovery/Discovery Example.fsx
@@ -0,0 +1,50 @@
+#r "System.Runtime.Serialization"
+#r "System.ServiceModel"
+#r "System.ServiceModel.Discovery"
+open System
+open System.ServiceModel
+open System.ServiceModel.Description
+open System.ServiceModel.Discovery
+
+
+[<ServiceContract>]
+type IEcho =
+    [<OperationContract>]
+    abstract Echo : string -> string
+
+
+type Echo() =
+    interface IEcho with
+        member this.Echo(s) =
+            sprintf "You said, \"%s\"" s
+
+
+let rand() =
+    (new Random()).Next(7000, 9999)
+
+let guid() =
+    Guid.NewGuid()
+
+let address = sprintf "net.tcp://localhost:%O/%O" <| rand() <| guid()
+let host = new ServiceHost(typeof<Echo>, new Uri(address))
+host.AddDefaultEndpoints()
+// Make Discoverable
+host.Description.Behaviors.Add(new ServiceDiscoveryBehavior())
+host.Description.Endpoints.Add(new UdpDiscoveryEndpoint())
+host.Open()
+printfn "Service listening on %O\n" address
+
+let echoEndpoint = 
+    new DynamicEndpoint(
+        ContractDescription.GetContract(typeof<IEcho>),
+        new NetTcpBinding())
+let channelFactory = new ChannelFactory<IEcho>(echoEndpoint)
+let echoChannel = channelFactory.CreateChannel()
+printfn "Service found at %O\n" <| echoEndpoint.Address
+printfn "%s" <| echoChannel.Echo("Discovery!")
+
+(echoChannel :?> ICommunicationObject).Close()
+host.Close()
+
+
+
