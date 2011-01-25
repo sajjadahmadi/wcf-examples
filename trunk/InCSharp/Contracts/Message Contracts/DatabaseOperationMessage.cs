dd5e65ecac3bbf1122f1a5a2a617d7d92bf2ba4a
mark.alan.good@gmail.com <mark.alan.good@gmail.com@7ff4561e-8701-11dd-9205-735403a6da14>
2011-01-25 14:40:35 +0000

    Cleanup

--- trunk/InCSharp/Contracts/Message Contracts/DatabaseOperationMessage.cs
+++ trunk/InCSharp/Contracts/Message Contracts/DatabaseOperationMessage.cs
@@ -1,32 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.ServiceModel.Channels;
+﻿using System.ServiceModel.Channels;
+using System.Xml;
 
 namespace WcfExamples.MessageContracts
 {
-    class DatabaseOperationMessage : Message
+    internal class DatabaseOperationMessage : Message
     {
         // TODO: See Transcender 1.4.2
 
+        private MessageHeaders _headers;
+        private MessageProperties _properties;
+        private MessageVersion _version;
         public string Username { get; set; }
         public string Password { internal get; set; }
         public string ConnectionString { get; set; }
         public string SQLStatement { get; set; }
 
-        private MessageProperties _properties;
-        private MessageHeaders _headers;
-        private MessageVersion _version;
-
         public override MessageHeaders Headers
         {
             get { return _headers; }
-        }
-
-        protected override void OnWriteBodyContents(System.Xml.XmlDictionaryWriter writer)
-        {
-            writer.WriteElementString("SQLStatement", SQLStatement);
         }
 
         public override MessageProperties Properties
@@ -37,6 +28,11 @@ namespace WcfExamples.MessageContracts
         public override MessageVersion Version
         {
             get { return _version; }
+        }
+
+        protected override void OnWriteBodyContents(XmlDictionaryWriter writer)
+        {
+            writer.WriteElementString("SQLStatement", SQLStatement);
         }
 
         // Other implementation omitted 
--- trunk/InCSharp/Contracts/Message Contracts/MessageHeader Attribute.cs
+++ trunk/InCSharp/Contracts/Message Contracts/MessageHeader Attribute.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Runtime.Serialization;
 using System.ServiceModel;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.ServiceModel.Description;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace WcfExamples.MessageContracts
 {
@@ -11,91 +10,112 @@ namespace WcfExamples.MessageContracts
     public class MessageHeaderAttributeExample
     {
         #region Contracts
+
+        #region Nested type: ContactInfo
+
         [DataContract]
-        class ContactInfo
+        private class ContactInfo
         {
-            [DataMember]
-            public string PhoneNumber;
-
-            [DataMember]
-            public string EmailAddress;
+            [DataMember] public string EmailAddress;
+            [DataMember] public string PhoneNumber;
         }
 
-        [MessageContract]
-        class ContactInfoRequestMessage
-        {
-            [MessageHeader]
-            public string LicenceKey;
-        }
+        #endregion
+
+        #region Nested type: ContactInfoRequestMessage
 
         [MessageContract]
-        class ContactInfoResponseMessage
+        private class ContactInfoRequestMessage
         {
-            [MessageBodyMember]
-            public ContactInfo ContactInfo;
+            [MessageHeader] public string LicenceKey;
         }
+
+        #endregion
+
+        #region Nested type: ContactInfoResponseMessage
+
+        [MessageContract]
+        private class ContactInfoResponseMessage
+        {
+            [MessageBodyMember] public ContactInfo ContactInfo;
+        }
+
+        #endregion
+
+        #region Nested type: ISomeService
 
         [ServiceContract]
-        interface ISomeService
+        private interface ISomeService
         {
             [OperationContract]
             [FaultContract(typeof(string))]
             ContactInfoResponseMessage GetContactInfo(
-                ContactInfoRequestMessage reqMsg);
+                    ContactInfoRequestMessage reqMsg);
         }
+
+        #endregion
+
         #endregion
 
         #region Service
-        class SomeService : ISomeService
+
+        private class SomeService : ISomeService
         {
+            #region ISomeService Members
+
             public ContactInfoResponseMessage GetContactInfo(ContactInfoRequestMessage reqMsg)
             {
                 if (reqMsg.LicenceKey != "some valid key")
-                {
-                    throw new FaultException<string>("Detail: Invalid license key: " + reqMsg.LicenceKey, "Reason: Invalid license key.");
-                }
+                    throw new FaultException<string>("Detail: Invalid license key: " + reqMsg.LicenceKey,
+                                                     "Reason: Invalid license key.");
 
-                ContactInfoResponseMessage respMsg =
-                    new ContactInfoResponseMessage();
+                var respMsg =
+                        new ContactInfoResponseMessage();
                 respMsg.ContactInfo = new ContactInfo();
                 respMsg.ContactInfo.EmailAddress = "some@email.com";
                 respMsg.ContactInfo.PhoneNumber = "555-555-5555";
 
                 return respMsg;
             }
+
+            #endregion
         }
+
         #endregion
 
         #region Host
-        ServiceHost host;
-        string address;
 
-        [TestInitialize()]
+        private string address;
+        private ServiceHost host;
+
+        [TestInitialize]
         public void MyTestInitialize()
         {
             host = new ServiceHost(typeof(SomeService));
-            address = "net.pipe://localhost/" + Guid.NewGuid().ToString();
+            address = "net.pipe://localhost/" + Guid.NewGuid();
             host.AddServiceEndpoint(typeof(ISomeService), new NetNamedPipeBinding(), address);
             host.Description.Behaviors.Find<ServiceDebugBehavior>().IncludeExceptionDetailInFaults = true;
             host.Open();
         }
 
-        [TestCleanup()]
+        [TestCleanup]
         public void MyTestCleanup()
         {
             if (host.State == CommunicationState.Opened)
                 host.Close();
         }
+
         #endregion
 
         [TestMethod]
         [ExpectedException(typeof(FaultException<string>), "Reason: Invalid license key.")]
         public void RequestWithInvalidKey()
         {
-            ISomeService proxy = ChannelFactory<ISomeService>.CreateChannel(new NetNamedPipeBinding(), new EndpointAddress(address));
+            var proxy = ChannelFactory<ISomeService>.CreateChannel(new NetNamedPipeBinding(),
+                                                                   new EndpointAddress(address));
             using (proxy as IDisposable)
             {
-                ContactInfoRequestMessage reqMsg = new ContactInfoRequestMessage();
+                var reqMsg = new ContactInfoRequestMessage();
                 reqMsg.LicenceKey = "some invalid key";
 
                 ContactInfoResponseMessage respMsg;
@@ -106,14 +126,13 @@ namespace WcfExamples.MessageContracts
         [TestMethod]
         public void RequestWithValidKey()
         {
-            ISomeService proxy = ChannelFactory<ISomeService>.CreateChannel(new NetNamedPipeBinding(), new EndpointAddress(address));
+            var proxy = ChannelFactory<ISomeService>.CreateChannel(new NetNamedPipeBinding(),
+                                                                   new EndpointAddress(address));
             using (proxy as IDisposable)
             {
-                ContactInfoRequestMessage reqMsg = new ContactInfoRequestMessage();
-                reqMsg.LicenceKey = "some valid key";
+                var reqMsg = new ContactInfoRequestMessage { LicenceKey = "some valid key" };
 
-                ContactInfoResponseMessage respMsg;
-                respMsg = proxy.GetContactInfo(reqMsg);
+                var respMsg = proxy.GetContactInfo(reqMsg);
 
                 Assert.AreEqual("555-555-5555", respMsg.ContactInfo.PhoneNumber);
                 Assert.AreEqual("some@email.com", respMsg.ContactInfo.EmailAddress);
