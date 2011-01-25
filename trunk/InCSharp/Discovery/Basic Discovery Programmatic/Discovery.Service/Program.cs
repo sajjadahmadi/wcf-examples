1c6b4826c7df1a421948771e5a25a27bf5c71c33
mark.alan.good@gmail.com <mark.alan.good@gmail.com@7ff4561e-8701-11dd-9205-735403a6da14>
2011-01-25 14:41:36 +0000

    Added Discovery examples

---
+++ trunk/InCSharp/Discovery/Basic Discovery Programmatic/Discovery.Service/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Discovery.Service
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(DiscoverableService), new Uri("http://localhost:8888/")))
+            {
+                host.Open();
+
+                Console.WriteLine("Press <ENTERT> to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ trunk/InCSharp/Discovery/Managed Discovery/Discovery.Client/DiscoveryClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Discovery.Client
+{
+    class DiscoveryClient : ClientBase<IDiscoverableService>, IDiscoverableService
+    {
+        public void ServiceOperation()
+        {
+            Channel.ServiceOperation();
+        }
+    }
+}
